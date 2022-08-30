b563d9a0eac6622b547ee864e0b8b219a28a25e2
luanerdy <luan.vini1@gmail.com>
2022-08-30 09:21:07 -0300

    reafactor: deleting not used actions

--- frontend/src/contexts/charactersContext.tsx
+++ frontend/src/contexts/charactersContext.tsx
@@ -22,46 +22,24 @@ export const initialState: CharactersState = {
 }
 
 export const ACTIONS = {
-	SET_COUNT: 'set_count',
 	SET_PREV_PAGE: 'set_prev_page',
-	SET_PAGE: 'set_page',
 	SET_INIT: 'set_init',
 	SET_PREV_NAME: 'set_prev_name',
-	CLEAR_COUNT: 'clear_count',
 	SET_FILMS: 'set_films',
-	ADD_FILMS: 'add_films',
-	CLEAR_FILMS: 'clear_films',
 	SET_PLANETS: 'set_planets',
-	ADD_PLANETS: 'add_planets',
-	CLEAR_PLANETS: 'clear_planets',
 	SET_STARSHIPS: 'set_starships',
-	ADD_STARSHIPS: 'add_starships',
-	CLEAR_STARSHIPS: 'clear_starships',
 	SET_VEHICLES: 'set_vehicles',
-	ADD_VEHICLES: 'add_vehicles',
-	CLEAR_VEHICLES: 'clear_vehicles',
 	SET_SPECIES: 'set_species',
-	ADD_SPECIES: 'add_species',
-	CLEAR_SPECIES: 'clear_species',
 	SET_GENDER: 'set_gender',
-	CLEAR_GENDER: 'clear_gender',
 	SET_SPECIE: 'set_specie',
-	CLEAR_SPECIE: 'clear_specie',
 	SET_FILM: 'set_film',
-	CLEAR_FILM: 'clear_film',
-	SET_TOTAL_PAGES: 'set_total_pages',
-	CLEAR_TOTAL_PAGES: 'clear_total_pages',
 	CLEAR_PAGE: 'clear_page',
 	SET_NAME: 'set_name',
-	CLEAR_NAME: 'clear_name',
 	NEXT_PAGE: 'next_page',
 	SET_OPEN: 'set_open',
 	CLEAR_OPEN: 'clear_open',
-	SET_CHARACTERS: 'set_characters',
 	ADD_CHARACTERS: 'add_characters',
-	CLEAR_CHARACTERS: 'clear_characters',
 	SET_ALL: 'set_all',
-	CLEAR_ALL: 'clear_all',
 }
 
 const reducer = (
@@ -69,12 +47,8 @@ const reducer = (
 	action: CharactersAction
 ): CharactersState => {
 	switch (action.type) {
-	case ACTIONS.SET_COUNT:
-		return { ...state, count: action?.payload?.count ?? 0 }
 	case ACTIONS.SET_PREV_PAGE:
 		return { ...state, prevPage: action?.payload?.prevPage ?? 0 }
-	case ACTIONS.SET_TOTAL_PAGES:
-		return { ...state, totalPages: action?.payload?.totalPages ?? 0 }
 	case ACTIONS.ADD_CHARACTERS:
 		return {
 			...state,
@@ -83,84 +57,16 @@ const reducer = (
 				...(action?.payload?.characters ?? []),
 			],
 		}
-	case ACTIONS.ADD_FILMS:
-		return {
-			...state,
-			films: [
-				...state.films,
-				...(action?.payload?.films ?? []),
-			],
-		}
-	case ACTIONS.ADD_PLANETS:
-		return {
-			...state,
-			planets: [
-				...state.planets,
-				...(action?.payload?.planets ?? []),
-			],
-		}
-	case ACTIONS.ADD_STARSHIPS:
-		return {
-			...state,
-			starships: [
-				...state.starships,
-				...(action?.payload?.starships ?? []),
-			],
-		}
-	case ACTIONS.ADD_VEHICLES:
-		return {
-			...state,
-			vehicles: [
-				...state.vehicles,
-				...(action?.payload?.vehicles ?? []),
-			],
-		}
-	case ACTIONS.ADD_SPECIES:
-		return {
-			...state,
-			species: [
-				...state.species,
-				...(action?.payload?.species ?? []),
-			],
-		}
-	case ACTIONS.CLEAR_CHARACTERS:
-		return { ...state, characters: [] }
-	case ACTIONS.CLEAR_FILMS:
-		return { ...state, films: [] }
-	case ACTIONS.CLEAR_PLANETS:
-		return { ...state, planets: [] }
-	case ACTIONS.CLEAR_STARSHIPS:
-		return { ...state, starships: [] }
-	case ACTIONS.CLEAR_VEHICLES:
-		return { ...state, vehicles: [] }
-	case ACTIONS.CLEAR_SPECIES:
-		return { ...state, species: [] }
-	case ACTIONS.CLEAR_GENDER:
-		return { ...state, gender: '' }
-	case ACTIONS.CLEAR_SPECIE:
-		return { ...state, specie: '' }
-	case ACTIONS.CLEAR_FILM:
-		return { ...state, film: '' }
-	case ACTIONS.CLEAR_NAME:
-		return { ...state, name: '' }
-	case ACTIONS.CLEAR_ALL:
-		return initialState
 	case ACTIONS.CLEAR_OPEN:
 		return { ...state, open: '' }
-	case ACTIONS.CLEAR_COUNT:
-		return { ...state, count: 0 }
 	case ACTIONS.SET_INIT:
 		return { ...state, init: action?.payload?.init ?? true }
-	case ACTIONS.CLEAR_TOTAL_PAGES:
-		return { ...state, totalPages: 0 }
 	case ACTIONS.CLEAR_PAGE:
 		return { ...state, prevPage: 1, page: 1 }
 	case ACTIONS.NEXT_PAGE:
 		return { ...state, prevPage: state.page, page: state.totalPages > state.page ? state.page + 1 : state.page }
 	case ACTIONS.SET_ALL:
 		return { ...state, ...action?.payload }
-	case ACTIONS.SET_CHARACTERS:
-		return { ...state, characters: action?.payload?.characters ?? [] }
 	case ACTIONS.SET_SPECIES:
 		return { ...state, species: action?.payload?.species ?? [] }
 	case ACTIONS.SET_FILMS:
--- frontend/src/hooks/useCharacters.ts
+++ frontend/src/hooks/useCharacters.ts
@@ -8,27 +8,25 @@ export const useCharacters = () => {
 	const genericPayloadAction = (type: string) => (payload: Partial<CharactersState>) => ({ type, payload })
 	const genericRawAction = (type: string) => () => ({ type })
 
-	const setAllAction = genericPayloadAction(ACTIONS.SET_ALL)
 	const setPrevPageAction = genericPayloadAction(ACTIONS.SET_PREV_PAGE)
-	const setNameAction = genericPayloadAction(ACTIONS.SET_NAME)
+	const setInitAction = genericPayloadAction(ACTIONS.SET_INIT)
 	const setPrevNameAction = genericPayloadAction(ACTIONS.SET_PREV_NAME)
-	const setGenderAction = genericPayloadAction(ACTIONS.SET_GENDER)
-	const setFilmAction = genericPayloadAction(ACTIONS.SET_FILM)
-	const addCharactersAction = genericPayloadAction(ACTIONS.ADD_CHARACTERS)
 	const setFilmsAction = genericPayloadAction(ACTIONS.SET_FILMS)
-	const addFilmsAction = genericPayloadAction(ACTIONS.ADD_FILMS)
-	const setSpecieAction = genericPayloadAction(ACTIONS.SET_SPECIE)
-	const setSpeciesAction = genericPayloadAction(ACTIONS.SET_SPECIES)
 	const setPlanetsAction = genericPayloadAction(ACTIONS.SET_PLANETS)
 	const setStarshipsAction = genericPayloadAction(ACTIONS.SET_STARSHIPS)
 	const setVehiclesAction = genericPayloadAction(ACTIONS.SET_VEHICLES)
-	const addSpeciesAction = genericPayloadAction(ACTIONS.ADD_SPECIES)
-	const setInitAction = genericPayloadAction(ACTIONS.SET_INIT)
+	const setSpeciesAction = genericPayloadAction(ACTIONS.SET_SPECIES)
+	const setGenderAction = genericPayloadAction(ACTIONS.SET_GENDER)
+	const setSpecieAction = genericPayloadAction(ACTIONS.SET_SPECIE)
+	const setFilmAction = genericPayloadAction(ACTIONS.SET_FILM)
+
 	const setOpenAction = genericPayloadAction(ACTIONS.SET_OPEN)
-	const clearAllAction = genericRawAction(ACTIONS.CLEAR_ALL)
-	const clearPageAction = genericRawAction(ACTIONS.CLEAR_PAGE)
-	const nextPageAction = genericRawAction(ACTIONS.NEXT_PAGE)
 	const clearOpenAction = genericRawAction(ACTIONS.CLEAR_OPEN)
+	const setAllAction = genericPayloadAction(ACTIONS.SET_ALL)
+	const setNameAction = genericPayloadAction(ACTIONS.SET_NAME)
+	const clearPageAction = genericRawAction(ACTIONS.CLEAR_PAGE)
+	const addCharactersAction = genericPayloadAction(ACTIONS.ADD_CHARACTERS)
+	const nextPageAction = genericRawAction(ACTIONS.NEXT_PAGE)
 
 	return {
 		state,
@@ -36,14 +34,11 @@ export const useCharacters = () => {
 		setAllAction,
 		nextPageAction,
 		addCharactersAction,
-		clearAllAction,
 		setNameAction,
 		setGenderAction,
 		setFilmsAction,
-		addFilmsAction,
 		setFilmAction,
 		setSpeciesAction,
-		addSpeciesAction,
 		setSpecieAction,
 		clearPageAction,
 		setOpenAction,
