64621c31825a06f29b6d43cf54321ff53d6dd861
rmls <rodrigomoreiralima@hotmail.com>
2022-12-22 16:09:22 -0300

    adicionando os métodos put e delete dos produtos

--- backend/desafio/src/main/java/br/com/desafio/controllers/ProductsController.java
+++ backend/desafio/src/main/java/br/com/desafio/controllers/ProductsController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +29,28 @@ public class ProductsController {
 		return service.findAll();
 	}
 	
+	@GetMapping(value = "/{id}",
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Products findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
+	}
+	
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Products create(@RequestBody Products product) {
 		return service.create(product);
 	}
+	
+	@PutMapping(value = "/{id}",consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Products> update(@RequestBody Products product, @PathVariable Long id) {
+		return service.update(product, id);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- backend/desafio/src/main/java/br/com/desafio/services/ProductsServices.java
+++ backend/desafio/src/main/java/br/com/desafio/services/ProductsServices.java
@@ -3,8 +3,10 @@ package br.com.desafio.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import br.com.desafio.exceptions.ResourceNotFoundException;
 import br.com.desafio.models.entities.Products;
 import br.com.desafio.models.repositories.ProductsRepository;
 
@@ -18,7 +20,34 @@ public class ProductsServices {
 		return repository.findAll();
 	}
 
+	public Products findById(Long id) {
+
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+	}
+
 	public Products create(Products product) {
 		return repository.save(product);
+	}
+
+	public ResponseEntity<Products> update(Products productDetails,Long id) {
+	
+		Products entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		entity.setName(productDetails.getName());
+		entity.setPrice(productDetails.getPrice());
+		entity.setDescription(productDetails.getDescription());
+
+		repository.save(entity);
+		
+		return ResponseEntity.ok(entity);
+	}
+
+	public void delete(Long id) {
+		var entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+		repository.delete(entity);
 	}
 }
