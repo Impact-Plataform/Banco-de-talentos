67fa04593c1bb5519ad15cb5e46f2e32cfd6b5ff
Uriel <urielbochi@gmail.com>
2022-12-25 22:10:43 -0300

    refactor: fetchSpecies improved

--- src/Services/fetchSpecies.tsx
+++ src/Services/fetchSpecies.tsx
@@ -1,45 +1,21 @@
 export async function getData(setDynamicState: React.Dispatch<React.SetStateAction<any>>, url: any)
 {
-    let nextPage:any = url;
-
-    let pages:any = 0;
-
-    let dataArray: any = []
-
-    dataArray.push({
-        value:'all',
-        label: 'All'
-    })
+    let nextPage = url;
+    let dataArray = [{ value:'all', label: 'All' }];
 
     while (nextPage) {
-        const res = await fetch(nextPage)
+        const res = await fetch(nextPage);
+        const { next, results } = await res.json();
 
-        const { next, results } = await res.json()
+        results.forEach((result: any) =>
+            dataArray.push({
+                value: result.name || result.title,
+                label: result.name || result.title
+            })
+        );
 
-        for (let i = 0; i < results.length; i++)
-        {
-
-            if (results[i].name)
-            {
-                dataArray.push({
-                    value: results[i].name,
-                    label: results[i].name
-                })
-            }
-            else
-            {
-                dataArray.push({
-                    value: results[i].title,
-                    label: results[i].title
-                })
-            }
-        }
-        
-        pages += 1;
-
-        nextPage = next
+        nextPage = next;
     }
 
-    setDynamicState(dataArray)
-
+    setDynamicState(dataArray);
 }
