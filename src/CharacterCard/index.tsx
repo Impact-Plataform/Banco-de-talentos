056ec983598252902ced2e73aeb7043dd1b3d73d
Uriel <urielbochi@gmail.com>
2022-12-26 13:16:31 -0300

    chore: character card (added more fields, css)

--- src/CharacterCard/index.tsx
+++ src/CharacterCard/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Character, CharacterTYPE } from '../Types';
 
-function CharacterCard({character} : Character) {
-    return(
-        <div>
-            <h3>{character.name}</h3>
+function CharacterCard({ character }: any) {
+    // console.log(character)
+    return (
+
+        <div className="card">
+            <img src={character.image} className='character__img' alt="" />
+            <div className="character__info">
+                <h2>NAME</h2>
+                <h3>{character.name}</h3>
+            </div>
+            <div className="character__info">
+                <h2>PLANET</h2>
+                <h3>{character.homeworld}</h3>
+            </div>
         </div>
     )
 }
