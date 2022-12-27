0d98ca5326592b2a07a2a042adbe33614a7b8cbf
Willian <willian.alves.b15@gmail.com>
2022-12-27 17:18:31 -0300

    feat: created component Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
+'use client'
+import '../styles/globals.css';
 import Link from 'next/link';
+import Header from '../components/header/Header';
 
 export default function Home() {
   return(
   <>
-    <p>
-      Willian
-    </p>
+    <Header/>
     <Link href="/detailsCaracter">detailsCaracter</Link>
-    </>
+  </>
   );
 }
