7850fad7ee6c539478fce9311e8612fb10489b6d
Willian <willian.alves.b15@gmail.com>
2022-12-28 10:51:13 -0300

    feat: crated components basic People and PeopleCard

---
+++ components/people/People.tsx
@@ -0,0 +1,10 @@
+import { PeopleContainer } from './people.style';
+
+
+export default function People() {
+  return(
+    <PeopleContainer>
+      
+    </PeopleContainer>
+  );
+}
---
+++ components/people/PeopleCard.tsx
@@ -0,0 +1,10 @@
+import { PeopleCardContainer } from './peopleCard.style';
+
+
+export default function PeopleCard() {
+  return(
+    <PeopleCardContainer>
+      
+    </PeopleCardContainer>
+  );
+};
