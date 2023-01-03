d0030207cc35e3f2758dbebee0cef14a32febadd
filipe <filipef.paulo@gmail.com>
2023-01-02 23:04:54 -0300

    Adiciona pagina inicial

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+
+export function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
