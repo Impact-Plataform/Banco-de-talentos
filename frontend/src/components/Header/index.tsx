d37aae4b47681772b0d82e701af78704af643ee4
filipe <filipef.paulo@gmail.com>
2023-01-02 23:48:39 -0300

    Adiciona toggle ao Header e estilização inicial

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import cx from 'classnames';
+import { useContext } from 'react';
+
+import { DarkModeContext, DarkModeContextData } from '../../providers/DarkMode';
+import { DarkModeToggle } from './DarkModeToggle';
+
 export function Header() {
-  return <div>SWAPI Client</div>;
+  const { darkMode } = useContext(DarkModeContext) as DarkModeContextData;
+
+  return (
+    <div
+      className={cx('fixed top-0 w-full flex flex-row justify-between py-4 px-10', {
+        'bg-gray-500': darkMode,
+        'bg-gray-600': !darkMode,
+      })}
+    >
+      SWAPI Client
+      <DarkModeToggle />
+    </div>
+  );
 }
