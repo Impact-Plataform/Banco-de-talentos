ed77b0dcba271016de619bc0e561c6cb9c25bc82
MarioFLS <mariofernandolsantos@gmail.com>
2023-01-04 16:17:04 -0300

    feat: Adicionar construtor na model

--- backend/ImpactAPI/Models/Product.cs
+++ backend/ImpactAPI/Models/Product.cs
@@ -13,5 +13,16 @@ namespace ImpactAPI.Models
         [Required(ErrorMessage = "O Preço é obrigatório")]
         [Range(0.0, Double.MaxValue, ErrorMessage = "O campo {0} deve ser maior que {1}")]
         public decimal Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
     }
 }
