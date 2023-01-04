8909a9d7bf7289ac9506471c5e345e0a7da44fff
MarioFLS <mariofernandolsantos@gmail.com>
2023-01-04 16:17:19 -0300

    feat: Contexto para o DB

---
+++ backend/ImpactAPI/Repository/ImpactContext.cs
@@ -0,0 +1,40 @@
+﻿using ImpactAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImpactAPI.Repository
+{
+    public class ImpactContext : DbContext
+    {
+        public ImpactContext(DbContextOptions<ImpactContext> options) : base(options)
+        {
+        }
+
+        protected ImpactContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().Property(p => p.Price).HasPrecision(6, 2);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product(1, "Computador", 3000.57m),
+                new Product(2, "Notebook", 4598.87m),
+                new Product(3, "Webcam", 600m),
+                new Product(4, "Microfone", 250m),
+                new Product(5, "HeadSet", 200m)
+                );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=127.0.0.1;Database=impact;User=SA;Password=Password12!;Encrypt=False");
+            }
+        }
+    }
+}
