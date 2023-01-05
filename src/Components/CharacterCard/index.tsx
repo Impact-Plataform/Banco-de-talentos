a1747bf3f48cd69c4909a2b015e7d4502d38fe8f
Uriel <urielbochi@gmail.com>
2023-01-05 20:06:12 -0300

    feat: character card

---
+++ src/Components/CharacterCard/index.tsx
@@ -0,0 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+import './index.css';
+
+function CharacterCard({ character }: any) {
+    const nav = useNavigate();
+
+
+    function holdClickedCharacterDetail() {
+        nav(`/${character.name}`)
+        localStorage.setItem('character', JSON.stringify(character));
+    }
+
+    return (
+
+        <div className="card" onClick={(e) => holdClickedCharacterDetail()}>
+            <img src={character.image} className='character__img' alt="" />
+            <div className="character__info">
+                <h2>NAME</h2>
+                <h3>{character.name}</h3>
+            </div>
+            <div className="character__info">
+                <h2>PLANET</h2>
+                <h3>{character.homeworld}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default CharacterCard;
