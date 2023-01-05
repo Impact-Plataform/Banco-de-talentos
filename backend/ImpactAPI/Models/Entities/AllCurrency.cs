c43f3e6646abf8fc2f4e960773026eb8873ce6bf
MarioFLS <mariofernandolsantos@gmail.com>
2023-01-04 21:55:34 -0300

    feat: Entidade de Moeda e AllCurrency para fazer a requisição correta da API

---
+++ backend/ImpactAPI/Models/Entities/AllCurrency.cs
@@ -0,0 +1,8 @@
+﻿namespace ImpactAPI.Models.Entities
+{
+    public class AllCurrency
+    {
+        public Currency USD { get; set; } = default!;
+        public Currency EUR { get; set; } = default!;
+    }
+}
--- backend/ImpactAPI/Models/Product.cs
+++ backend/ImpactAPI/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ImpactAPI.Models
 {
@@ -11,8 +13,14 @@ namespace ImpactAPI.Models
         public string Name { get; set; } = default!;
 
         [Required(ErrorMessage = "O Preço é obrigatório")]
-        [Range(0.0, Double.MaxValue, ErrorMessage = "O campo {0} deve ser maior que {1}")]
+        [Range(0.0, double.MaxValue, ErrorMessage = "O campo {0} deve ser maior que {1}")]
         public decimal Price { get; set; }
+
+        [NotMapped]
+        public decimal? PriceUSD { get; private set; }
+
+        [NotMapped]
+        public decimal? PriceEUR { get; private set; }
 
         public Product()
         {
@@ -23,6 +31,14 @@ namespace ImpactAPI.Models
             Id = id;
             Name = name;
             Price = price;
+        }
+
+        public void ConverteValue(decimal USD, decimal EUR)
+        {
+            // Os valores quando deixados com duas casas, são arredondados. Ou seja, eles podem valer 1 centavo a mais. Mas como é um preço possível
+            // a diferença de 1 centavo acaba não sendo um problema tão grande.
+            PriceUSD = decimal.Round(Price / USD, 2);
+            PriceEUR = decimal.Round(Price / EUR, 2);
         }
     }
 }
