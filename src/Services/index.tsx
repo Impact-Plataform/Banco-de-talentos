3b94fa453756fcf0b73dee8b7e0a1f6c6afc466d
Uriel <urielbochi@gmail.com>
2023-01-05 20:01:40 -0300

    refactor/feat: main service refactored | filter added

--- src/Services/index.tsx
+++ src/Services/index.tsx
@@ -1,73 +1,91 @@
 import axios from 'axios';
-import { useState } from 'react';
-import photos from '../assets/Photos/photos.json'
+import { Dispatch } from 'react';
+import characterInfo from '../assets/MoreAboutCharacters/charactersInfo.json'
+import { CharacterTYPE } from '../Types';
+import { filterData } from './filterOptions';
 
-export async function getCharacters(pagNumber : number, setCharactersList: React.Dispatch<React.SetStateAction<any>>) {
+export async function handleCharacters(pagNumber: number, setCharactersList: React.Dispatch<React.SetStateAction<any>>, filterOptions: any) {
+    axios.get(`https://swapi.py4e.com/api/people/?page=${pagNumber}`)
+        .then(async (response) => {
 
-    const options: any = { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
+            let charactersArray: CharacterTYPE[] = [];
 
+            response.data.results.forEach((character: CharacterTYPE) => charactersArray.push(character));
+            
+            // caso exista algum filtro ativo
+            if (filterOptions.gender || filterOptions.specie || filterOptions.film) {
+                let nextPage = response.data.next;
+                // enquanto existir próxima página (enquanto next page não for null)
+                while (nextPage) 
+                {
+                    //pegue a resposta
+                    const res = await fetch(nextPage);
+                    const { next, results } = await res.json();
 
-    axios.get(`https://swapi.dev/api/people/?page=${pagNumber}`)
-    .then(async (response) => {
-
-        let processedItems = 0;
-
-        // pega item por item
-        response.data.results.forEach(async (character:any, index: number, array: any) => {
-            // console.log(character)
-
-            // Despacha o homeworld do personagem
-            const homeworldResponse = await axios.get(character.homeworld);
-            character.homeworld = homeworldResponse.data.name;
-
-            //Recupera os filmes de todos os personagens
-            const filmsPromises = character.films.map((film: any) => axios.get(film));
-            //Despacha todos os filmes
-            const filmsResponses = await Promise.all(filmsPromises);
-            character.films = filmsResponses.map((response) => response.data.title);
-
-            const speciesPromises = character.species.map((film: any) => axios.get(film));
-            let speciesResponses:any = await Promise.all(speciesPromises);
-
-            //a espécie human não está presente na API, fiz essa verificação para adiciona-lá e poder tratar com filtros.
-            if (speciesResponses.length > 0)
-            {
-                character.species = speciesResponses.map((response:any) => response.data.name);    
+                    // adicione ao array
+                    results.forEach(async (result: CharacterTYPE) => {
+                        charactersArray.push(result)
+                    });
+                    // passa para a próxima página
+                    nextPage = next;
+                }    
+                promisesDealer(charactersArray, setCharactersList, filterOptions)
+            } else {
+                promisesDealer(charactersArray, setCharactersList, filterOptions)
             }
-            else
-            {
-                character.species = ['Human']
-            }
-
-            let characterImage = photos.find((item:any) => item.name === character.name)
-
-            character.image = characterImage?.image
-
-            processedItems += 1;
-
-            if (processedItems === array.length)
-            {
-                setCharactersList(response.data.results)
-            }
-
         });
-    });
 }
 
-export async function getTotalPages(setTotalPages: React.Dispatch<React.SetStateAction<any>>, totalPages : number)
-{
-    let nextPage:any = 'https://swapi.dev/api/people/';
 
-    let pages:any = 0;
+async function promisesDealer(charactersList: CharacterTYPE[], setCharactersList: Dispatch<any>, filterOptions: CharacterTYPE) {
+
+    let processedItems = 0;
+
+    charactersList.forEach(async (character: any, index: number, array: any) => {
+        let characterMoreInfo = characterInfo.find((item: any) => item.name === character.name)
+
+        // Despacha o homeworld do personagem
+        const homeworldResponse = await axios.get(character.homeworld);
+        character.homeworld = homeworldResponse.data.name;
+
+        //Recupera os filmes de todos os personagens
+        const filmsPromises = character.films.map((film: any) => axios.get(film));
+        //Despacha todos os filmes
+        const filmsResponses = await Promise.all(filmsPromises);
+        character.films = filmsResponses.map((response) => response.data.title);
+
+        const speciesPromises = character.species.map((specie: any) => axios.get(specie));
+        let speciesResponses: any = await Promise.all(speciesPromises);
+        character.species = speciesResponses.map((response: any) => response.data.name);
+
+
+        character.image = characterMoreInfo?.image;
+        character.quote = characterMoreInfo?.quote;
+
+        processedItems += 1;
+
+        if (processedItems === array.length) {
+            if (filterOptions.gender || filterOptions.specie || filterOptions.film)  {
+                filterData(charactersList, filterOptions, setCharactersList);
+            } else {
+                setCharactersList(charactersList);
+            }
+        }
+
+    });
+
+}
+
+export async function getTotalPages(setTotalPages: React.Dispatch<React.SetStateAction<any>>, totalPages: number) {
+    let nextPage: any = 'https://swapi.py4e.com/api/people/';
+
+    let pages: any = 0;
 
     while (nextPage) {
         const res = await fetch(nextPage)
 
         const { next, results } = await res.json()
-        
+
         pages += 1;
 
         nextPage = next
@@ -77,14 +95,3 @@ export async function getTotalPages(setTotalPages: React.Dispatch<React.SetState
     setTotalPages(totalPages + pages);
 
 }
-
-
-
-// async function fetchCharactersSubCategories(character: any, category: string, setCharactersList:any)
-// {
-
-//     const filmsPromises = character[category].map((film: any) => axios.get(film));
-//     const filmsResponses = await Promise.all(filmsPromises);
-//     character.films = filmsResponses.map((response) => response.data.title);
- 
-// }
