d4e6a4134e379d2e6aedea2a1fcb526f321e2e8a
Uriel <urielbochi@gmail.com>
2023-01-05 20:02:57 -0300

    chore: filter logic

---
+++ src/Services/filterOptions.tsx
@@ -0,0 +1,21 @@
+export async function filterData(characters: any, filterOptions: any, setCharactersList: React.Dispatch<React.SetStateAction<any>>) {
+    const filteredArray = characters.filter((person: any) => {        
+
+        if (filterOptions.gender && (person.gender.toLowerCase() !== filterOptions.gender.toLowerCase()) && filterOptions.gender !== 'all') {
+            return false;
+        }
+
+        if (filterOptions.specie && ((!person.species.includes(filterOptions.specie)) && filterOptions.specie !== 'all')) {
+            return false;
+        }
+
+        if (filterOptions.film && (!person.films.includes(filterOptions.film)) && filterOptions.film !== 'all') {
+            return false;
+        }
+
+
+        return true;
+    });
+
+    setCharactersList(filteredArray);
+}
