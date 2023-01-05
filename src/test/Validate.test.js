694171f90582dbfd128ac28fbf5426b637c5b882
Luany Oliveira <96268553+luanyoliveira16@users.noreply.github.com>
2023-01-05 17:28:07 -0300

    estabelecendo a pasta correta

--- src/test/Validate.test.js
+++
@@ -1,9 +0,0 @@
-import Validacao from "../services/Validacoes.js"
-
-test("Se o symbol é R$", () => {
-    expect(Validacao.valSymbol("R$")).toBe(true);
-})
-
-test("Se currency é BRL", () => {
-    expect(Validacao.valCurrency("BRL")).toBe(true);
-})
