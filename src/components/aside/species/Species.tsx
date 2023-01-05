b7ccd827e784f24a2a488c6b2d9dee51444276bd
Willian <willian.alves.b15@gmail.com>
2023-01-05 10:22:29 -0300

    feat: rendering the species names

--- src/components/aside/species/Species.tsx
+++ src/components/aside/species/Species.tsx
@@ -1,7 +1,8 @@
 'use client'
+import { v4 as uuidv4 } from 'uuid';
+import { useContext, useEffect, useState } from 'react';
 import { SpeciesContainer } from './species.style';
 import { getAllSpecies } from '../../../services/aside.service';
-import { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../../../contexts/contextProvider';
 import { ISpecie } from '../../../types/Species.types';
 import { storeAllSpecies } from '../../../store/store';
@@ -10,7 +11,7 @@ import { storeAllSpecies } from '../../../store/store';
 export default function Species() {
   const { isLoading } = useContext(AppContext);
   const [species, setSpecies] = useState<ISpecie[]>([]);
-
+  const [limit, setLimit] = useState(10)
 
   useEffect(() => {
     storeAllSpecies(getAllSpecies, setSpecies)
@@ -18,7 +19,13 @@ export default function Species() {
 
   return(
     <SpeciesContainer>
-
+      <h3>Espécies</h3>
+      <ul>
+        { species.slice(0, limit).map((specie) => (
+          <li key={ uuidv4() }>{specie.name}</li>
+        )) }
+      </ul>
+      <p>Mostrar mais</p>
     </SpeciesContainer>
   );
 };
