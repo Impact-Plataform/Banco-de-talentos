31eacdc89d882c708b4476f09b29a9c0c9573ef3
Uriel <urielbochi@gmail.com>
2023-01-05 20:02:08 -0300

    chore: some new types

--- src/Types/index.tsx
+++ src/Types/index.tsx
@@ -3,11 +3,28 @@ export interface CharacterTYPE  {
     height: number;
     mass: number;
     setCharactersList: React.Dispatch<React.SetStateAction<string>>;
-    character: object;
     homeworld: string;
     films: Array<string>;
+    charactersList: string[];
+    image: string;
+    birth_year: string;
+    gender: string;
+    quote: string;
+    specie: string;
+    film: string;
+    species: string;
 }
 
 export interface Character {
    character: CharacterTYPE;
 }
+
+export interface IInputWrapperProps {
+    label?: string;
+    required?: boolean;
+    minimizedLabel?: boolean;
+    description?: string;
+    error?: string;
+    wrapperStyle?: React.CSSProperties;
+    children?: JSX.Element|JSX.Element[];
+  }
