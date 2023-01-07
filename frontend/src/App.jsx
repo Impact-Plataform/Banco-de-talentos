443e6a57bbdac729cd65ff5bdd43db3fa69594c7
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-07 18:48:52 -0300

    🪛refactor: realizada pequenas atualizações no código: import, cor, escrita

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 import Header from "./components/Header/Header";
+import Section from "./components/Section/Section";
 
 function App() {
   return (
-    
+    <div>
       <Header />
-    
+      <Section/>
+      
+    </div>
   );
 }
 
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav class="navbar bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand">SWAPI</a>
-        <form class="d-flex" role="search">
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand">SWAPI</a>
+        <form className="d-flex" role="search">
           <input
-            class="form-control me-2"
+            className="form-control me-2"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button class="btn btn-outline-success" type="submit">
+          <button className="btn btn-outline-success" type="submit">
             Search
           </button>
         </form>
