ea51006ba9094a296d1c70e5845d22d10ff28755
filipe <filipef.paulo@gmail.com>
2023-01-08 00:26:40 -0300

    Simplifica as chamadas da API para uma unica função com chamada inteligente

--- frontend/src/api/SWAPI.ts
+++ frontend/src/api/SWAPI.ts
@@ -1,17 +1,13 @@
 import axios from 'axios';
 
-export function getAllPeople(page = 1, search = '') {
-  const result = axios
-    .get('https://swapi.dev/api/people/', { params: { page, search } })
-    .then((res) => res.data.results)
-    .catch((err) => err.detail);
-  return result;
+interface paramsPros {
+  page?: number;
+  search?: string;
 }
 
-export function getAllFilms() {
-  const result = axios
-    .get('https://swapi.dev/api/films/')
-    .then((res) => res.data.results)
+export function getByPath(path: string, params: paramsPros = {}) {
+  return axios
+    .get(`https://swapi.dev/api/${path}/`, { params: { ...params } })
+    .then((res) => ({ ...res.data, ok: true }))
     .catch((err) => err.detail);
-  return result;
 }
