aa720915cfc21579e09edf009aecca16937144af
brunonasc74 <bruno.hpn74@gmail.com>
2023-01-09 05:27:46 -0300

    swaggerUI implemented, documentation on route /api-docs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = require('./router');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./config/swagger.json');
 
 const app = express();
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.get('/', (_, res) => res.redirect('/api-docs'));
 
 router(app);
 
