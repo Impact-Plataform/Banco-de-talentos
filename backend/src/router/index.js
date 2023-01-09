0f401778d71e1def3041893478e34434bbad7695
brunonasc74 <bruno.hpn74@gmail.com>
2023-01-09 05:38:46 -0300

    Route for resource not found created

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,7 +1,9 @@
 const products = require('./productsRoute');
 const currencies = require('./currenciesRoute');
+const notFound = require('./notFoundRoute');
 
 module.exports = (app) => {
 	app.use(products);
 	app.use(currencies);
+	app.use(notFound);
 };
