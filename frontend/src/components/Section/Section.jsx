497286a2880c787c32752d4e6c699c35d3719ac1
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-09 19:30:03 -0300

    ⚙️perf: importado novos compenentes e novas resp. da req. no card

--- frontend/src/components/Section/Section.jsx
+++ frontend/src/components/Section/Section.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import api from "../../service/api";
 import Card from "../Card/Card";
+import Pagination from "../Pagination/Pagination";
+import Spinner from "../Spinner/Spinner";
 import style from "./Section.module.css";
 
 const Section = () => {
@@ -15,33 +17,40 @@ const Section = () => {
         console.error("ops! ocorreu um erro" + err);
       });
   }, [page]);
-  console.log(dados, "resultados")
-  
-  
+  console.log(dados, "resultados");
+
   const nextPage = () => {
     setPage(page + 1);
-  }
+  };
 
   const previousPage = () => {
     setPage(page - 1);
-  }
+  };
 
   return (
     <section className={style.section}>
-      {dados ? dados.map((item, index) => {
-        return(
-          <Card
-                key={index}
-                nome={item.name}
-                altura={item.height}
-                peso={item.mass}
-                corDoCabelo={item.hair_color}
-                roupa={item.skin_color}
-                corDosOlhos={item.eye_color}
-                genero={item.gender}
-              />
-        )
-      }) : "Loading..."}
+      {dados ? (
+        dados.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              nome={item.name}
+              altura={item.height}
+              peso={item.mass}
+              corDoCabelo={item.hair_color}
+              roupa={item.skin_color}
+              corDosOlhos={item.eye_color}
+              genero={item.gender}
+              especie={item.species}
+              filmes={item.films}
+            />
+          );
+        })
+      ) : (
+        <Spinner />
+      )}
+
+      <Pagination onClick={nextPage} />
     </section>
   );
 };
