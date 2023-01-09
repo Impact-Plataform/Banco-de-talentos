2b913929b533905837bd13f90930fe73c70e243e
Uriel <urielbochi@gmail.com>
2023-01-09 20:41:14 -0300

    chore/fix: trying to fix a bug in deploy

--- src/Services/index.tsx
+++ src/Services/index.tsx
@@ -44,11 +44,11 @@ async function promisesDealer(charactersList: CharacterTYPE[], setCharactersList
             if (filterOptions.gender || !filterOptions.species || filterOptions.film) {
                 setTimeout(() => {
                     filterData(charactersList, filterOptions, setCharactersList)
-                }, 1000)
+                }, 1500)
             } else {
                 setTimeout(() => {
                     setCharactersList(charactersList);
-                }, 1000)
+                }, 1500)
             }
         }
     }
