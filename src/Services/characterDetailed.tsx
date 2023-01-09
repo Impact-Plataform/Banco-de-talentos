5dab3ff926feabaa4b23aabd43aa933b79efbfef
Uriel <urielbochi@gmail.com>
2023-01-09 16:31:24 -0300

    refactor: function getDetailedCharacter refactored

--- src/Services/characterDetailed.tsx
+++ src/Services/characterDetailed.tsx
@@ -1,53 +1,30 @@
 import axios from "axios";
-import characterInfo from '../assets/MoreAboutCharacters/charactersInfo.json'
-import filmsInfo from '../assets/MoreAboutCharacters/films.json'
 import { CharacterTYPE } from "../Types";
+import { characterDetailsHandler } from "./characterDetailsHandler";
 
-export async function getDetailedCharacter(character: any, setCharactersList: any) {
+
+export async function getDetailedCharacter(character: any, setCharactersList: React.Dispatch<React.SetStateAction<string | CharacterTYPE[]>>) {
     axios.get(`https://swapi.py4e.com/api/people/?search=${character}`).then((response => {
-
         let characterHolder: CharacterTYPE[] = [];
-        response.data.results.forEach((character: any) => characterHolder.push(character));
+        response.data.results.forEach((character: CharacterTYPE) => characterHolder.push(character));
 
+        // se houver pelo menos um personagem
         if (response.data.count > 0) {
-            characterHolder.forEach(async (character: any) => {
-                const characterMoreInfo = characterInfo.find((item: any) => item.name === character.name)
-                let films: any = [];
-
-                const homeworldResponse = await axios.get(character.homeworld);
-                const speciesResponse = await axios.get(character.species);
-
-                const filmsPromises = character.films.map((film: any) => axios.get(film));
-                const filmsResponses = await Promise.all(filmsPromises);
-
-
-                setTimeout(() => {
-                    filmsResponses.map((response) => films.push(filmsInfo.find((item: any) => item.name === response.data.title)));
-                    character.films = films;
-
-
-                }, 100)
-
-                character.image = characterMoreInfo?.image;
-                character.quote = characterMoreInfo?.quote;
-                character.description = characterMoreInfo?.description;
-
-                character.homeworld = homeworldResponse.data.name
-                character.species = speciesResponse.data.name
-
-
-            });
-
+            getCharacterData(characterHolder);
             setTimeout(() => {
                 setCharactersList(characterHolder);
-
             }, 350)
-
         }
-        if (response.data.count === 0) {
+        else {
+            // Caso não haja um personagem
             setCharactersList('Character not found');
         }
-
-
     }))
+}
+
+async function getCharacterData(charactersList: CharacterTYPE[]) {
+    charactersList.forEach(async (character: any) => {
+        characterDetailsHandler(character);
+
+    });
 }
