6815746d17f687b79fe304d8927b778d0975f1f4
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2023-01-10 20:29:00 -0300

    chore: create load products -  factory, controllers and route

--- backend/src/main/routes/create-products-routes.ts
+++ backend/src/main/routes/create-products-routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { makeCreateProductController } from '../factories/create-product-factory'
+import { makeCreateProductController } from '../factories/products/create-product-factory'
+import { makeLoadProductsController } from '../factories/products/load-products-factory'
 
 const productsRoutes = Router()
 
 productsRoutes.post('/products', makeCreateProductController)
+productsRoutes.get('/products', makeLoadProductsController)
 
 export { productsRoutes }
