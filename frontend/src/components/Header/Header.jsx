69b96808721dbfb03238c7e2501a0f83548e6188
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-10 02:14:49 -0300

    ✨feat: add btn de seleçao para filtrar a pesquisa

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,20 +1,50 @@
-import React from "react";
-import logo from "../../assets/images/jedi-logo.svg"
-import style from "./Header.module.css"
-const Header = () => {
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/images/star-wars-logo.svg";
+import style from "./Header.module.css";
+import api from "../../service/api";
+
+const Header = ({ filmes }) => {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    api
+      .get(`films/`)
+      .then((response) => {
+        setFilms([response.data.results]);
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+  console.log(films, "filmes");
+
   return (
     <nav className="navbar bg-body-tertiary">
       <div className="container-fluid">
-      <a className="navbar-brand"><img src={logo} className={style.logo}/>SWAPI - The Star Wars API</a>
+        <a className="navbar-brand">
+          <img src={logo} className={style.logo} />
+        </a>
         <form className="d-flex" role="search">
           <input
             className="form-control me-2"
             type="search"
-            placeholder="Search"
+            placeholder="Pesquisar"
             aria-label="Search"
           />
-          <button className="btn btn-outline-success" type="submit">
-            Search
+          <select class="form-select me-2" aria-label="Select">
+            <option selected>Filtrar</option>
+            <option value="1">Especie</option>
+            <option value={filmes}>Filmes</option>
+            <option value="3">Genero</option>
+          </select>
+          {films.map((result) => {
+            return (
+              filmes = result.title
+              );
+          })}
+
+          <button className="btn btn-success" type="submit">
+            Buscar
           </button>
         </form>
       </div>
