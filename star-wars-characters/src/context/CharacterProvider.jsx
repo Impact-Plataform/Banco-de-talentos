a5d3b2b578855c6c9059dcdb5e0a4bcc43f2c9d7
genesis-g101 <gyreplicant@gmail.com>
2023-01-10 19:03:51 -0300

    criando e testando a chamada de api

--- star-wars-characters/src/context/CharacterProvider.jsx
+++ star-wars-characters/src/context/CharacterProvider.jsx
@@ -1,5 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { CharacterContext } from './CharacterContext';
 
 export const CharacterProvider = ({ children }) => {
+  const [allCharacters, setAllCharacters] = useState([]);
+
+  // estados simples
+  const [loading, setLoading] = useState([]);
+
+  // chama a api
+  const getAllCharacters = async () => {
+    let url = 'https://swapi.dev/api/people';
+
+    try {
+      console.log('chamando o recurso people');
+
+      let characters = [];
+      while (url) {
+        const response = await axios(url);
+        console.log(response.data);
+
+        const { next, results } = response.data;
+        url = next;
+        characters = [...characters, ...results];
+      }
+
+      console.log('terminou a chamada');
+      setAllCharacters(characters);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllCharacters();
+  }, []);
+  console.log('people: ', allCharacters);
   return <CharacterContext.Provider value={{ number: 0 }}>{children}</CharacterContext.Provider>;
 };
