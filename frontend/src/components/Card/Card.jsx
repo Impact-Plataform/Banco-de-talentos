08f50a75427fd42653c2888bb45ccd1fe643475a
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-10 02:13:47 -0300

    💄feat: estilização bootstrap do card

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -9,27 +9,29 @@ const Card = ({
   corDosOlhos,
   especie,
   genero,
-  filmes
+  filmes,
 }) => {
   return (
-    <div className="row w-100">
-  <div className="col-sm-4 mb-3 mb-sm-0 p-4">
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{nome}</h5>
-        <p className="card-text">{genero}</p>
-        <p className="card-text">{altura}</p>
-        <p className="card-text">{peso}</p>
-        <p className="card-text">{corDoCabelo}</p>
-        <p className="card-text">{corDosOlhos}</p>
-        <p className="card-text">{roupa}</p>
-        <p className="card-text">{especie}</p>
-        <p className="card-text">{filmes}</p>
-        <a href="#" className="btn btn-primary">Ver mais</a>
+    
+      <div class="col">
+        <div class="card h-100">
+          <div className="card-body">
+            <h5 className="card-title">{nome}</h5>
+            <p className="card-text">{genero}</p>
+            <p className="card-text">{altura}</p>
+            <p className="card-text">{peso}</p>
+            <p className="card-text">{corDoCabelo}</p>
+            <p className="card-text">{corDosOlhos}</p>
+            <p className="card-text">{roupa}</p>
+            <p className="card-text">{especie}</p>
+            <p className="card-text">{filmes}</p>
+            <a href="#" className="btn btn-primary">
+              Ver mais
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  </div>
+    
   );
 };
 
