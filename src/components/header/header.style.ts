b7568432345b6df8373f85e06b8d6efd39654fb5
Willian <willian.alves.b15@gmail.com>
2023-01-11 11:45:56 -0300

    feat(styles): new style Header

--- src/components/header/header.style.ts
+++ src/components/header/header.style.ts
@@ -4,9 +4,13 @@ import styled from 'styled-components';
 export const HeaderContainer = styled.header`
   width: 100%;
   border: 2px solid black;
-  display: flex;
-  justify-content: space-between;
   background: rgb(27, 26, 23);
+  
+  a {
+    display: flex;
+    justify-content: space-between;
+  }
+
   img {
     height: auto;
   }
