32aecdfc8263179333b084f2a8637539f5cbc57d
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-11 00:28:15 -0300

    💄style: alteração de estilo bootstrap do card

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Card = ({
+  imageId,
   nome,
   altura,
   peso,
@@ -16,7 +17,8 @@ const Card = ({
       <div className="col">
         <div className="card h-100">
           <div className="card-body">
-            <h5 className="card-title">{nome}</h5>
+          <img src={imageId} className="card-img-top" alt="personagens"/>
+            <h5 className="card-title text-center">{nome}</h5>
             <p className="card-text">{genero}</p>
             <p className="card-text">{altura}</p>
             <p className="card-text">{peso}</p>
@@ -25,7 +27,7 @@ const Card = ({
             <p className="card-text">{roupa}</p>
             <p className="card-text">{especie}</p>
             <p className="card-text">{filmes}</p>
-            <a href="#" className="btn btn-primary">
+            <a href="#" className="btn btn-primary d-flex justify-content-center">
               Ver mais
             </a>
           </div>
