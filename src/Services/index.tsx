bb108feedf708930a477f312c3abebff7c580262
Uriel <urielbochi@gmail.com>
2023-01-12 01:26:47 -0300

    refactor: promises dealer refactored

--- src/Services/index.tsx
+++ src/Services/index.tsx
@@ -1,16 +1,14 @@
 import axios from 'axios';
-import { Dispatch } from 'react';
 import { CharacterTYPE } from '../Types';
-import { getDetailedCharacter } from './characterDetailed';
 import { characterDetailsHandler } from './characterDetailsHandler';
 import { filterData } from './filterOptions';
 
-export async function handleCharacters(pagNumber: number, filterOptions: any, characterArray:any) {
+export async function handleCharacters(pagNumber: number, filterOptions: any, characterArray: CharacterTYPE[], searchValue: string) {
     const response = await axios.get(`https://swapi.py4e.com/api/people/?page=${pagNumber}`);
 
     response.data.results.forEach((character: CharacterTYPE) => characterArray.push(character));
 
-    if (filterOptions.gender || filterOptions.specie || filterOptions.film) {
+    if (filterOptions.gender || filterOptions.specie || filterOptions.film || searchValue.length > 0) {
         let nextPage = response.data.next;
         while (nextPage) {
 
@@ -26,33 +24,30 @@ export async function handleCharacters(pagNumber: number, filterOptions: any, ch
     } else {
         return characterArray;
     }
-    
+
 }
 
-export async function promisesDealer(charactersList: CharacterTYPE[], setCharactersList: Dispatch<CharacterTYPE[]>, filterOptions: any, searchValue:any) {
-    let processedItems:number = 0
-    
-    charactersList.forEach(async (character: any, index: number, array: any) => {
+export async function promisesDealer(charactersList: CharacterTYPE[], setCharactersList: React.Dispatch<React.SetStateAction<string | CharacterTYPE[]>>, filterOptions: any, searchValue: string) {
+    let processedItems: number = 0
+
+    charactersList.forEach(async (character: CharacterTYPE, index: number, array: any) => {
         await characterDetailsHandler(character);
 
         processedItems++;
 
         if (processedItems === array.length) {
             if (filterOptions.gender || !filterOptions.species || filterOptions.film) {
-               let item = await filterData(charactersList, filterOptions, searchValue)
-
-               if (item.length === 0 ) {
-                  await getDetailedCharacter(searchValue, setCharactersList, filterOptions, searchValue)
-               }
-
-               Promise.resolve(item).then((response) => {
-                  setCharactersList(response);
-               })
+                let item = await filterData(charactersList, filterOptions, searchValue)
+                Promise.resolve(item).then((response) => {
+                    console.log(response)
+                    if(response.length === 0 ) {}
+                    setCharactersList(response);
+                })
 
             }
         }
 
-})
+    })
 }
 
 export async function getTotalPages(setTotalPages: React.Dispatch<React.SetStateAction<number>>, totalPages: number) {
