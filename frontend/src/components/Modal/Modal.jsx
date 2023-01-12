0dbbdd6c2de9e7c4866d71c63b41d48eef6eceb9
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-12 18:13:26 -0300

    ✨feat: add atributos

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-import svg from "../../assets/images/stormtrooper.svg"
+import svg from "../../assets/images/stormtrooper.svg";
 
-const Modal = () => {
+const Modal = ({
+  idModal,
+  nome,
+  genero,
+  altura,
+  peso,
+  corDoCabelo,
+  corDosOlhos,
+  CorDaPele,
+  especie,
+  anoDeNascimento,
+  planeta,
+  filmes,
+}) => {
   return (
     <div>
       <button
@@ -9,6 +22,7 @@ const Modal = () => {
         className="btn btn-primary d-flex justify-content-center"
         data-bs-toggle="modal"
         data-bs-target="#staticBackdrop"
+        key={idModal}
       >
         Ver mais
       </button>
@@ -26,7 +40,7 @@ const Modal = () => {
           <div className="modal-content">
             <div className="modal-header">
               <h1 className="modal-title fs-5" id="staticBackdropLabel">
-                Nome do personagem
+                {nome}
               </h1>
               <button
                 type="button"
@@ -35,10 +49,24 @@ const Modal = () => {
                 aria-label="Close"
               ></button>
             </div>
-            <div className="modal-body">atributos</div>
+            <div className="modal-body">
+              <p className="card-text">Gênero: <strong>{genero}</strong></p>
+              <p className="card-text">Altura: <strong>{altura} m </strong></p>
+              <p className="card-text">Peso: <strong>{peso} kg </strong></p>
+              <p className="card-text">Cor do cabelo: <strong>{corDoCabelo}</strong></p>
+              <p className="card-text">Cor dos olhos: <strong>{corDosOlhos}</strong></p>
+              <p className="card-text">Cor da pele: <strong>{CorDaPele}</strong></p>
+              <p className="card-text">Espécie: <strong>{especie}</strong></p>
+              <p className="card-text">Ano de Nascimento: <strong>{anoDeNascimento}</strong></p>
+              <p className="card-text">Planeta Natal: <strong>{planeta}</strong></p>
+              <p className="card-text">Filmes em que aparece: <strong>{filmes}</strong></p>
+            </div>
             <div className="modal-footer">
-              <img src={svg} alt="icone stormtrooper" style={{width: 3 + 'em'}}/>
-              
+              <img
+                src={svg}
+                alt="icone stormtrooper"
+                style={{ width: 3 + "em" }}
+              />
             </div>
           </div>
         </div>
