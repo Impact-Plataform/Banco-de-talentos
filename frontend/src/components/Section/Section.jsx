df90a203fd8cc43faedf9372d116aa692bf74b89
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-12 18:14:43 -0300

    ♻️refactor: refatoração card

--- frontend/src/components/Section/Section.jsx
+++ frontend/src/components/Section/Section.jsx
@@ -9,11 +9,16 @@ import img from "../../assets/images/personagens/19.jpg";
 const Section = () => {
   const [page, setPage] = useState(1);
   const [array, setArray] = useState([]);
+  console.log(array, "personagens");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true)
     api
       .get(`/people/?page=${page}`)
-      .then((response) => {setArray(response.data.results)})
+      .then((response) => {
+        setArray(response.data.results);
+      }, setLoading(false))
       .catch((err) => {
         console.error("ops! ocorreu um erro" + err);
       });
@@ -27,37 +32,41 @@ const Section = () => {
     setPage(page - 1);
   };
 
-
   return (
     <section className={style.section}>
       <div className="container-md">
-      <div className="row row-cols-1 row-cols-md-5 g-3 p-3">
-      {array ? (
-        array.map((personagem, index) => {
-          return (
-            <Card
-              key={index}
-              imageId={img}
-              nome={personagem.name}
-              // genero={item.gender}
-              // altura={item.height}
-              // peso={item.mass}
-              // corDoCabelo={item.hair_color}
-              // corDosOlhos={item.eye_color}
-              // roupa={item.skin_color}
-            />
-          );
-        })
-      ) : (
-        <Spinner />
-      )}
-      </div>
+        <div className="row row-cols-1 row-cols-md-5 g-3 p-3">
+          {array ? (
+            array.map((personagem, index) => {
+              return (
+                <Card
+                  key={index}
+                  id={personagem.id}
+                  imageId={img}
+                  nome={personagem.name}
+                  genero={personagem.gender}
+                  altura={personagem.height}
+                  peso={personagem.mass}
+                  corDoCabelo={personagem.hair_color}
+                  corDosOlhos={personagem.eye_color}
+                  corDaPele={personagem.skin_color}
+                  especie={personagem.species}
+                  anoDeNascimento={personagem.birth_year}
+                  planeta={personagem.homeworld}
+                  filmes={personagem.films}
+                />
+              );
+            })
+          ) : (
+            <Spinner/>
+          )}
+        </div>
       </div>
       <div className="container-fluid p-2">
-      <Pagination anterior={previousPage} proxima={nextPage} />
+        <Pagination anterior={previousPage} proxima={nextPage} />
       </div>
-      
     </section>
+    
   );
 };
 
