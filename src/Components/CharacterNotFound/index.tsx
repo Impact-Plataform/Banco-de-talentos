481e00066eb6b953a348cf21db63ce218fb161b4
Uriel <urielbochi@gmail.com>
2023-01-12 14:52:43 -0300

    style: not found css

--- src/Components/CharacterNotFound/index.tsx
+++ src/Components/CharacterNotFound/index.tsx
@@ -1,11 +1,14 @@
 import notFoundGif from '../../assets/notFound.gif'
 import './index.css'
 import Background from '../Background';
+import { useNavigate } from 'react-router-dom';
 
 
 
 
 function CharacterNotFound() {
+
+    const nav = useNavigate();
 
     return (
         <div>
@@ -13,6 +16,7 @@ function CharacterNotFound() {
             <div className='characterNotFound__container'>
                 <img src={notFoundGif} alt="Darth Vader looking to the left. His armor color change to purple and glitch to other colors." />
                     <h1 className="characterNotFound__text">Character not found</h1>
+                    <div onClick={() => nav('/')} className='back__button'>Return to main page</div>
             </div>
         </div>
     )
