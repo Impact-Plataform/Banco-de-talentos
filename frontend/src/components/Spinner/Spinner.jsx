5f106319e81de92c3eb2b7b730ee257ce9225002
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-12 18:15:09 -0300

    💄feat: estilização

--- frontend/src/components/Spinner/Spinner.jsx
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 const Spinner = () => {
+  
   return (
     <button className="btn btn-primary" type="button" disabled>
       <span
-        className="spinner-grow spinner-grow-sm"
+        className="spinner-grow spinner-grow-sm me-2"
         role="status"
         aria-hidden="true"
       ></span>
-      Loading...
+       Carregando...
     </button>
+    
   );
 };
 
