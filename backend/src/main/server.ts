9ba96427faa5ed7fc40dccff6ab7e250d7a0f7e2
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2023-01-11 21:23:55 -0300

    fix: add process.env.PORT in file server.ts

--- backend/src/main/server.ts
+++ backend/src/main/server.ts
@@ -1,7 +1,7 @@
 import { app } from './app'
 
-const port = 3333
+const PORT = process.env.PORT || 3333
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${port}/docs`)
 })
