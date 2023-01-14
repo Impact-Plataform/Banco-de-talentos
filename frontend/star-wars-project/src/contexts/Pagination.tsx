50b14a8e61e8d0889cb7b6f7e2d77347c25b623c
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-14 11:59:08 -0300

    feat: create pagination context

---
+++ frontend/star-wars-project/src/contexts/Pagination.tsx
@@ -0,0 +1,52 @@
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
+import { ProviderProps } from "./interfaces/providerProps";
+
+interface PaginationContextProps {
+  paginationNumbers: number[];
+  selectedPaginationNumber: number;
+  nextPage: () => void;
+  previousPage: () => void;
+}
+
+const PaginationContext = createContext<PaginationContextProps>(
+  {} as PaginationContextProps
+);
+
+export const usePageNumber = () => {
+  const context = useContext(PaginationContext);
+
+  return context;
+};
+
+export const PaginationProvider = ({ children }: ProviderProps) => {
+  const paginationNumbers = [1, 2, 3, 4, , 5, 6, 7, 8] as number[];
+  const [number, setNumber] = useState<number>(0);
+
+  const selectedPaginationNumber: number = paginationNumbers[number];
+
+  const previousPage = () => {
+    number > 0 && setNumber(number - 1);
+  };
+  const nextPage = () => {
+    number < 7 && setNumber(number + 1);
+  };
+
+  return (
+    <PaginationContext.Provider
+      value={{
+        selectedPaginationNumber,
+        paginationNumbers,
+        previousPage,
+        nextPage,
+      }}
+    >
+      {children}
+    </PaginationContext.Provider>
+  );
+};
