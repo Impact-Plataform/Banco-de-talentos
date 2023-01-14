41c3a05503d5d432c8a95512107ce155275f0090
Marlon <marlon14br@gmail.com>
2023-01-14 01:03:51 -0300

    Config API ASYNC + GET + rotas :wrench:

--- frontend/Projeto-FrontEnd-Impact/src/Components/Cards/Cards/Index.jsx
+++ frontend/Projeto-FrontEnd-Impact/src/Components/Cards/Cards/Index.jsx
@@ -5,44 +5,50 @@ import api from "../../../services/Api";
 
 export const Cards = () => {
 
-
+    const [page, atPage] = useState(1)
     const [people, setPeople] = useState([])
 
+    const showMore = () => {
+        atPage(page + 1)
+      }
 
-    useEffect(() => {
-        fetch("https://swapi.dev/api/people")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.results)
-                setPeople(data.results)
-            })
+      const getApi = async () => {
 
+        try {
+            let res = await fetch(`https://swapi.dev/api/people/?page=${page}`)
+            let data = await res.json()
+            setPeople([...people, ...data.results])
+        } 
+        catch (error) {
+      console.log(error)
+    }
+  }
 
-    }, [])
-
+  const Cardpeople =   people.map(people => {
     return (
+        <li key={people?.url}>
+            <Link to={`/details/${String(people.url).split("/")[5]}`}><div className="card"><h1>Gender: {people?.gender}</h1>
+                <p>Planeta :{people?.homeworld}</p>
+                <p>Pagina : {people?.url}</p>
+            </div></Link>
+            <span>Nome: {people?.name}</span>
 
-        <StyledCardUl>{
-            people.map(people => {
-                return (
-                    <li key={people?.url}>
-                       { /* console.log("id :" + String(people.url).split("/")[5]) */}
-                        <Link to={`/details/${String(people.url).split("/")[5]}`}><div className="card"><h1>Gender: {people?.gender}</h1>
-                            <p>Planeta :{people?.homeworld}</p>
-                            <p>Pagina : {people?.url}</p>
-                        </div></Link>
-                        <span>Nome: {people?.name}</span>
-
-                    </li>
-
-                )
-            })
-
-        }
-
-        </StyledCardUl>
+        </li>
 
     )
+})
+
+useEffect(() => {
+    getApi()
+  }, [page])
+
+    return (
+        <>
+        <StyledCardUl>
+        {Cardpeople}
+        </StyledCardUl>
+        <button onClick={showMore}>Mostre mais</button>
+    </>)
 }
 
 
