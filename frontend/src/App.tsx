6cba34a16949eaf2742f6ec24b153a1f36c186ac
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2023-01-15 20:17:08 -0300

    feat: implement charactercardlist component in home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,9 @@
 import { Routes } from "./routes";
-import { getPeople } from "./api/people";
-import { useEffect, useState } from "react";
-import { Button, CharacterCardList } from "./components";
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-
-  const handleCharacters = async () => {
-    const charactersResponse = await getPeople({ page: 2 });
-    setCharacters(charactersResponse);
-  };
-  useEffect(() => {
-    handleCharacters();
-  }, []);
 
   return (
     <div>
-      <Button text="Next" onClick={() => {console.log("May the force be with you!")}}/>
-      <CharacterCardList characters={characters} />
       <Routes />
     </div>
   );
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,3 +1,21 @@
+import { getPeople } from "../../api/people";
+import { useEffect, useState } from "react";
+import { CharacterCardList } from "../../components";
+
 export const Home = () => {
-  return <>Home</>;
+  const [characters, setCharacters] = useState([]);
+
+  const handleCharacters = async () => {
+    const charactersResponse = await getPeople({ page: 2 });
+    setCharacters(charactersResponse);
+  };
+  useEffect(() => {
+    handleCharacters();
+  }, []);
+
+  return (
+    <>
+      <CharacterCardList characters={characters} />
+    </>
+  );
 };
