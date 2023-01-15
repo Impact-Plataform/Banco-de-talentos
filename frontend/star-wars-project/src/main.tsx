0976c4f02eef4baf47b13a6414e1155d49180ac8
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-15 18:30:13 -0300

    feat: add IndexProvider to main

--- frontend/star-wars-project/src/main.tsx
+++ frontend/star-wars-project/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { IndexProvider } from "./contexts/IndexProvider";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <IndexProvider>
+      <App />
+    </IndexProvider>
   </React.StrictMode>
-)
+);
