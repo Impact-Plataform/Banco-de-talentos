724e57b621f900a3402fa6b5e96043615b20eebf
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-15 18:20:58 -0300

    feat: create filter context to filter people

---
+++ frontend/star-wars-project/src/contexts/filter.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+import { ProviderProps } from "./interfaces/providerProps";
+
+interface FilterContextProps {
+  dataFiltered: any[];
+  filterData: (word: string, filterFunction: (word: string) => any[]) => void;
+  // filterData: (word: string) => void;
+}
+
+const filterContext = createContext<FilterContextProps>(
+  {} as FilterContextProps
+);
+
+export const useFilter = () => {
+  const context = useContext(filterContext);
+
+  return context;
+};
+
+export const FilterProvider = ({ children }: ProviderProps) => {
+  const [dataFiltered, setDataFiltered] = useState<any[]>([]);
+  console.log("dataFiltered -> ", dataFiltered);
+
+  const filterData = (word: string, filterFunction: (word: string) => any[]) => {
+    const filtered = filterFunction(word)
+    setDataFiltered(filtered)
+  }
+
+  // const filterData = (word: string) => {
+  //   const wordLower = word.toLowerCase();
+  //   setDataFiltered([wordLower]);
+  // };
+
+  return (
+    <filterContext.Provider value={{ dataFiltered, filterData }}>
+      {children}
+    </filterContext.Provider>
+  );
+};
