26b239fe1fe2c047d52e1bb0fd1e89daffef3c4e
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-15 18:20:10 -0300

    feat: create apiData context to get star wars people

---
+++ frontend/star-wars-project/src/contexts/ApiData.tsx
@@ -0,0 +1,41 @@
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from "react";
+import { api } from "../services/api";
+import { ProviderProps } from "./interfaces/providerProps";
+import { usePageNumber } from "./Pagination";
+
+interface ContextProps {
+  data: any[];
+}
+
+const ApiInfoContext = createContext<ContextProps>({} as ContextProps);
+
+export const useAPiInfo = () => {
+  const context = useContext(ApiInfoContext);
+  return context;
+};
+
+export const ApiinfoProvider = ({ children }: ProviderProps) => {
+  const [data, setData] = useState<any[]>([]);
+
+  const { selectedPaginationNumber } = usePageNumber();
+
+  useEffect(() => {
+    api
+      .get(`/people/?page=${selectedPaginationNumber}`)
+      .then((res) => setData(res.data.results));
+  }, []);
+
+  return (
+    <ApiInfoContext.Provider value={{ data }}>
+      {children}
+    </ApiInfoContext.Provider>
+  );
+};
