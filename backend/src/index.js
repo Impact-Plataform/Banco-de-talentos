f24ee814b9e4d9c767f119c18053836ad4370e17
Lucas Lima <limalucks@hotmail.com>
2023-01-16 16:06:59 -0300

    Rotas e controladores da tag Currency implementadas na API com sucesso!

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const rotasProdutos = require('./rotas/produtos')
+const rotasCotacoes = require('./rotas/cotacoes')
 
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('../swagger.json')
@@ -8,5 +9,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use(express.json())
 app.use(rotasProdutos)
+app.use(rotasCotacoes)
 
 app.listen(3000)
