ba13add47a25321ae762348b84dcce223d67308e
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-15 23:19:47 -0300

    feat: create Star wars title component

---
+++ frontend/star-wars-project/src/components/starWarsTitle/StarWarsTitle.tsx
@@ -0,0 +1,6 @@
+import "./stylesStarWarsTitle.css";
+
+export const StarWarsTitle = () => {
+  // remember: insert function to sent to characters page when click in title
+  return <h1 className="title">STAR WARS</h1>;
+};
