576dee0d9054d279db9c4eeec1c62cd24cb971eb
Lucas Lima <limalucks@hotmail.com>
2023-01-16 20:53:57 -0300

    Implementados os controladores das rotas de Produto, tudo funcionando perfeitamente!

--- backend/src/controladores/produtos.js
+++ backend/src/controladores/produtos.js
@@ -1,7 +1,8 @@
 const { v4: uuid } = require('uuid')
-let { produtos } = require('../Banco de dados/dados')
+const api = require('../Banco de dados/api')
+let produtos = []
 
-const cadastrar = (req, res) => {
+const cadastrar = async (req, res) => {
   const { nome, descricao, valorEmReal } = req.body
   const id = uuid()
 
@@ -16,20 +17,33 @@ const cadastrar = (req, res) => {
   }
 
   if (nome && descricao && valorEmReal) {
-    const novoProduto = {
-      nome,
-      id,
-      descricao,
-      valor: {
-        BRL: `R$${valorEmReal}` /*,
-        "USD": `$${valorEmDolar}`,
-        "EUR": `€${valorEmEuro}`*/
+    try {
+      const url = 'https://economia.awesomeapi.com.br/all'
+      const { data } = await api.get(url)
+
+      const cotacaoDolar = data.USD.bid
+      const cotacaoEuro = data.EUR.bid
+
+      const valorEmDolar = parseFloat(valorEmReal) / parseFloat(cotacaoDolar)
+      const valorEmEuro = parseFloat(valorEmReal) / parseFloat(cotacaoEuro)
+
+      const novoProduto = {
+        nome,
+        id,
+        descricao,
+        valor: {
+          BRL: valorEmReal,
+          USD: valorEmDolar.toFixed(2),
+          EUR: valorEmEuro.toFixed(2)
+        }
       }
+
+      produtos.push(novoProduto)
+
+      return res.status(200).json(novoProduto)
+    } catch (error) {
+      res.send({ error: error.message })
     }
-
-    produtos.push(novoProduto)
-
-    return res.status(200).json(novoProduto)
   } else {
     return res.status(403).json({
       mensagem: 'Preencha todos os dados corretamente!'
@@ -37,19 +51,85 @@ const cadastrar = (req, res) => {
   }
 }
 
-const listar = (req, res) => {
-  return res.status(200).json(produtos)
+const listar = async (req, res) => {
+  let produtosAtualizados = []
+
+  try {
+    const url = 'https://economia.awesomeapi.com.br/all'
+    const { data } = await api.get(url)
+
+    const cotacaoDolar = data.USD.bid
+    const cotacaoEuro = data.EUR.bid
+
+    for (let i = 0; i < produtos.length; i++) {
+      const valorEmReal = produtos[i].valor.BRL
+      const valorEmDolar = valorEmReal / parseFloat(cotacaoDolar)
+      const valorEmEuro = valorEmReal / parseFloat(cotacaoEuro)
+
+      const novoProduto = {
+        nome: produtos[i].nome,
+        id: produtos[i].id,
+        descricao: produtos[i].descricao,
+        valor: {
+          BRL: valorEmReal,
+          USD: valorEmDolar.toFixed(2),
+          EUR: valorEmEuro.toFixed(2)
+        }
+      }
+
+      produtosAtualizados.push(novoProduto)
+    }
+
+    produtos = produtosAtualizados
+
+    if (!produtos.length) {
+      return res.status(200).json({
+        mensagem: 'Nenhum produto cadastrado!'
+      })
+    }
+
+    return res.status(200).json(produtos)
+  } catch (error) {
+    res.send({ error: error.message })
+  }
 }
 
-const detalhar = (req, res) => {
+const detalhar = async (req, res) => {
   const { id } = req.params
 
-  const produtoExistente = produtos.find(produtos => {
+  let produtoExistente = produtos.find(produtos => {
     return produtos.id === id
   })
 
   if (produtoExistente) {
-    return res.status(200).json(produtoExistente)
+    try {
+      const url = 'https://economia.awesomeapi.com.br/all'
+      const { data } = await api.get(url)
+
+      const cotacaoDolar = data.USD.bid
+      const cotacaoEuro = data.EUR.bid
+
+      const valorEmReal = produtoExistente.valor.BRL
+      const valorEmDolar = valorEmReal / parseFloat(cotacaoDolar)
+      const valorEmEuro = valorEmReal / parseFloat(cotacaoEuro)
+
+      let produtoAtualizado = {
+        nome: produtoExistente.nome,
+        id: produtoExistente.id,
+        descricao: produtoExistente.descricao,
+        valor: {
+          BRL: valorEmReal,
+          USD: valorEmDolar.toFixed(2),
+          EUR: valorEmEuro.toFixed(2)
+        }
+      }
+
+      produtoExistente = produtoAtualizado
+
+      return res.status(200).json(produtoAtualizado)
+    } catch (error) {
+      res.send({ error: error.message })
+    }
   } else {
     return res.status(404).json({
       mensagem: 'Produto não encontrado!'
@@ -57,7 +137,7 @@ const detalhar = (req, res) => {
   }
 }
 
-const editar = (req, res) => {
+const editar = async (req, res) => {
   const { id } = req.params
   const { nome, descricao, valorEmReal } = req.body
 
@@ -79,22 +159,37 @@ const editar = (req, res) => {
         }
       }
 
-      const produtoEditado = {
-        nome,
-        id,
-        descricao,
-        valor: {
-          BRL: `R$${valorEmReal}` /*,
-    "USD": `$${valorEmDolar}`,
-    "EUR": `€${valorEmEuro}`*/
+      try {
+        const url = 'https://economia.awesomeapi.com.br/all'
+        const { data } = await api.get(url)
+
+        const cotacaoDolar = data.USD.bid
+        const cotacaoEuro = data.EUR.bid
+
+        const valorEmDolar = valorEmReal / parseFloat(cotacaoDolar)
+        const valorEmEuro = valorEmReal / parseFloat(cotacaoEuro)
+
+        let produtoAtualizado = {
+          nome,
+          id,
+          descricao,
+          valor: {
+            BRL: valorEmReal,
+            USD: valorEmDolar.toFixed(2),
+            EUR: valorEmEuro.toFixed(2)
+          }
         }
+
+        const i = produtos.indexOf(produtoExistente)
+
+        produtos[i] = produtoAtualizado
+
+        return res.status(200).json({
+          mensagem: 'Produto editado com sucesso!'
+        })
+      } catch (error) {
+        res.send({ error: error.message })
       }
-
-      produtoExistente = produtoEditado
-
-      return res.status(200).json({
-        mensagem: 'Produto editado com sucesso!'
-      })
     } else {
       return res.status(403).json({
         mensagem: 'Preencha todos os campos!'
@@ -107,7 +202,7 @@ const editar = (req, res) => {
   }
 }
 
-const excluir = (req, res) => {
+const excluir = async (req, res) => {
   const { id } = req.params
 
   const produtoExistente = produtos.find(produtos => {
