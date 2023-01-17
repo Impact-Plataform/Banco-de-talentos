d28647c478e74ffb28d38ecc8ef0599ea0052e21
Marlon <marlon14br@gmail.com>
2023-01-17 17:17:57 -0300

    Config options, Form & style :wrench:

--- frontend/Projeto-FrontEnd-Impact/src/Components/Form/Index.jsx
+++ frontend/Projeto-FrontEnd-Impact/src/Components/Form/Index.jsx
@@ -1,17 +1,23 @@
-import { GetFilms } from "../../services/Api";
+import { GetFilms, GetSpecies } from "../../services/Api";
 import { StyledForm } from "./Style"
 import { useEffect, useState } from "react";
+import { Button } from "../Button/Index";
 
 
 
 
 export const Form = () => {
 
-  const [filmes, setFilmes] = useState([]); // ATRIBUR A API UM ESTADO
+
+
+// REQUISIÇÃO DOS FILMES //
+const [filmes, setFilmes] = useState([]); // ATRIBUR A API UM ESTADO
+
 
 
 useEffect(() => {
   getFilmes()
+  getEspecies()
 }, [])
 
   async function getFilmes(){
@@ -29,40 +35,58 @@ useEffect(() => {
   const CardFilmes =   filmes.map(filmes => {
     return (
         
-       <option>{filmes.title}</option>
+       <option key={filmes.title}>{filmes.title}</option>
     
     )
 })
 
+// -------------- // --------------------- // ---------------- // 
+
+const [especies, setEspecies] = useState([]); // ATRIBUR A API UM ESTADO
+
+  async function getEspecies(){
+      try{
+          const response = await GetSpecies()
+          let data = response
+          setEspecies([...especies, ...data.results])
+          console.log(especies)
+      }catch(e){
+          console.log(e)
+      }
+  }
+
+
+  const CardEspecies =   especies.map(especies => {
+    return (
+        
+       <option >{especies.name}</option>
+    
+    )
+})
+
+// --------------------------------------- //
 
 
 
   return (
     <StyledForm>
       <form method="post" action="">
-        <select>
+        <select name="Filtros">
+          <optgroup label="Gênero">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="n/a">N/A</option>
+          </optgroup>
           <optgroup label="Filmes">
             {CardFilmes}
           </optgroup>
-          <optgroup label="Grupo 2">
-            <option>Opção 2.1</option>
-            <option>Opção 2.2</option>
-            <option>Opção 2.1</option>
-            <option>Opção 2.2</option>
-            <option>Opção 2.1</option>
-            <option>Opção 2.2</option>
+          <optgroup label="Especies">
+            {CardEspecies}
           </optgroup>
-          <optgroup label="Grupo 3">
-            <option>Opção 3.1</option>
-            <option>Opção 3.2</option>
-            <option>Opção 3.3</option>
-            <option>Opção 3.1</option>
-            <option>Opção 3.2</option>
-            <option>Opção 3.3</option>
-          </optgroup>
+          
         </select>
         <input type="submit" value="Enviar" />
-        <input type="text" placeholder="Nome do personagem" />
+        <input type="text" placeholder="Filtrar ou buscar nome" />
 
         <input type="reset" value="Cancelar" />
       </form>
