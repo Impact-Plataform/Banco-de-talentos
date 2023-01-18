6f4761d6444d4dda08b0aeb2daac69330ae50684
Bruno Bispo <brunobispoo25@gmail.com>
2023-01-17 23:04:52 -0300

    Implementado o caso de uso de criação de produtos no controller.

--- src/controllers/product/createProductController.ts
+++ src/controllers/product/createProductController.ts
@@ -2,15 +2,16 @@ import { Product } from '@prisma/client';
 import { Request, Response } from 'express';
 import { prismaClient } from '../../database/PrismaClient';
 import { PrismaProductRepository } from '../../repositories/prismaProductRepository';
+import { CreateProduct } from '../../use-cases/createProduct';
 
 const prismaProductRepository = new PrismaProductRepository(prismaClient);
+const createProduct = new CreateProduct(prismaProductRepository);
 
 export class createProductController {
 	async execute(req: Request<unknown, unknown, Omit<Product, 'id'>>, res: Response) {
 		const { amount, name, value } = req.body;
 
-		const product = await prismaProductRepository.create({amount, value, name});
-
+		const product = await createProduct.create({amount, name, value});
 		return res.status(201).json({ product });
 	}
 }
--- src/router/productRouter.ts
+++ src/router/productRouter.ts
@@ -22,7 +22,7 @@ const validateProductId = new GetProductMiddleware();
 const currencyValues = new GetCurrencyValues();
 
 productRouter.get('/', currencyValues.get, getProducts.execute);
-productRouter.post('/', validateBodyFields.execute, registerProduct.execute);
+productRouter.post('/', registerProduct.execute);
 productRouter.get('/:id', validateProductId.execute, currencyValues.get, getProduct.execute);
 productRouter.put('/:id', validateProductId.execute,  validateBodyFields.execute, updateProduct.execute);
 productRouter.delete('/:id', validateProductId.execute, deleteProduct.execute);
