5b20ccb1154d345d3270f797f08307cb0a934bd3
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-17 22:27:40 -0300

    ♻️refactor: retirado import sem uso e formataçao do codigo

--- frontend/src/components/Section/Section.jsx
+++ frontend/src/components/Section/Section.jsx
@@ -3,14 +3,13 @@ import api from "../../service/api";
 import Card from "../Card/Card";
 import Pagination from "../Pagination/Pagination";
 import style from "./Section.module.css";
-import img from "../../assets/images/personagens/19.jpg";
 
 const Section = () => {
   const [page, setPage] = useState(1);
   const [array, setArray] = useState([]);
-  
+
   useEffect(() => {
-      api
+    api
       .get(`/people/?page=${page}`)
       .then((response) => {
         setArray(response.data.results);
@@ -22,14 +21,14 @@ const Section = () => {
 
   const nextPage = () => {
     setPage(page + 1);
-    if(page == 9){
+    if (page == 9) {
       return setPage(1);
     }
   };
 
   const previousPage = () => {
     setPage(page - 1);
-    if(page == 1) {
+    if (page == 1) {
       return setPage(1);
     }
   };
@@ -38,25 +37,45 @@ const Section = () => {
     <section className={style.section}>
       <div className="container-md">
         <div className="row row-cols-1 row-cols-md-5 g-3 p-3">
-            {array.map((personagem) => {
-              return (
-                <Card
-                  key={personagem.url}
-                  imageId={img}
-                  nome={personagem.name}
-                  genero={personagem.gender == "n/a" ? "indisponível" : personagem.gender} 
-                  altura={personagem.height}
-                  peso={personagem.mass == "unknown" ? "indisponível" : personagem.mass}
-                  corDoCabelo={personagem.hair_color == "n/a" ? "Não possui" : personagem.hair_color}
-                  corDosOlhos={personagem.eye_color}
-                  corDaPele={personagem.skin_color == "unknown" ? "indisponível" : personagem.skin_color}
-                  especie={personagem.species}
-                  anoDeNascimento={personagem.birth_year == "unknown" ? "indisponível" : personagem.birth_year}
-                  planeta={personagem.homeworld}
-                  filmes={personagem.films}
-                />
-              );
-            })}
+          {array.map((personagem) => {
+            return (
+              <Card
+                key={personagem.url}
+                nome={personagem.name}
+                genero={
+                  personagem.gender == "n/a"
+                    ? "indisponível"
+                    : personagem.gender
+                }
+                altura={personagem.height}
+                peso={
+                  personagem.mass == "unknown"
+                    ? "indisponível"
+                    : personagem.mass
+                }
+                corDoCabelo={
+                  personagem.hair_color == "n/a"
+                    ? "Não possui"
+                    : personagem.hair_color
+                }
+                corDosOlhos={personagem.eye_color}
+                corDaPele={
+                  personagem.skin_color == "unknown"
+                    ? "indisponível"
+                    : personagem.skin_color
+                }
+                especie={personagem.species}
+                anoDeNascimento={
+                  personagem.birth_year == "unknown"
+                    ? "indisponível"
+                    : personagem.birth_year
+                }
+                planeta={personagem.homeworld}
+                filmes={personagem.films}
+                url={personagem.url}
+              />
+            );
+          })}
         </div>
       </div>
       <div className="container-fluid p-2">
