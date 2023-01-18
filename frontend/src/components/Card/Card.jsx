2da76ebb642c2c4e10de56b7051fd4bc688c0a09
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-17 22:29:18 -0300

    ✨feat: nova const que pega o id parametro do url de cada personagem

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Modal from "../Modal/Modal";
 
 const Card = ({
-  imageId,
   nome,
   genero,
   altura,
@@ -14,23 +13,40 @@ const Card = ({
   anoDeNascimento,
   planeta,
   filmes,
-  url
+  url,
 }) => {
+  const idFoto = url.slice(29, -1);
+
+  const fotoCard = `../personagens/${idFoto}.jpg`;
 
   return (
-    
-      <div className="col">
-        <div className="card h-100">
-          <div className="card-body">
-          <img src={imageId} className="card-img-top" alt="personagens"/>
-            <h5 className="card-title text-center">{nome}</h5>
-            
-            <Modal nome={nome} genero={genero} altura={altura} peso={peso} corDoCabelo={corDoCabelo} corDosOlhos={corDosOlhos} CorDaPele={CorDaPele} especie={especie} anoDeNascimento={anoDeNascimento} planeta={planeta} filmes={filmes}/>
-          
-          </div>
-          
+    <div className="col">
+      <div className="card h-100">
+        <div className="card-body">
+          <img
+            style={{ height: 280 + "px" }}
+            src={fotoCard}
+            className="card-img-top"
+            alt="personagens"
+          />
+          <h5 className="card-title text-center">{nome}</h5>
+
+          <Modal
+            nome={nome}
+            genero={genero}
+            altura={altura}
+            peso={peso}
+            corDoCabelo={corDoCabelo}
+            corDosOlhos={corDosOlhos}
+            CorDaPele={CorDaPele}
+            especie={especie}
+            anoDeNascimento={anoDeNascimento}
+            planeta={planeta}
+            filmes={filmes}
+          />
         </div>
       </div>
+    </div>
   );
 };
 
