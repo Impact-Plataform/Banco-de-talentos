756251cd0cd1791075e6ede70e4248883bacc01b
Bruno Bispo <brunobispoo25@gmail.com>
2023-01-17 23:03:12 -0300

    Criado o caso de uso de criação de produtos.

---
+++ src/use-cases/createProduct.ts
@@ -0,0 +1,22 @@
+import { Product } from '@prisma/client';
+import { ProductRepository } from '../repositories/productRepository';
+import { BadRequestError } from '../utils/apiErrors';
+import { validateBody } from '../yupschemas/productSchema';
+
+export class CreateProduct {
+	constructor(private productRepository: ProductRepository) {}
+
+	async create(props: Omit<Product, 'id'>) {
+		await validateBody.validate(props);
+
+		const existsProduct = await this.productRepository.findByName(props.name);
+
+		if(existsProduct) {
+			throw new BadRequestError('Produto já cadastrado');
+		}
+
+		const product = await this.productRepository.create(props);
+
+		return product;
+	}
+}
