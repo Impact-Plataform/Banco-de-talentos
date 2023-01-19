7578bee41d1d0e7efbea41e713437f771bc6ac6a
rmls <rodrigomoreiralima@hotmail.com>
2023-01-19 00:51:03 -0300

    feat: adicionando constraints de validacao

--- backend/src/main/java/com/impact/project/model/Product.java
+++ backend/src/main/java/com/impact/project/model/Product.java
@@ -3,6 +3,7 @@ package com.impact.project.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,10 +26,12 @@ public class Product extends RepresentationModel<Product> {
 
     @Column(nullable = false)
     @NotBlank
+    @Size(max = 60)
     private String name;
     
     @Column
     @NotBlank
+    @Size(max = 255)
     private String description;
     
     @Column(nullable = false)
