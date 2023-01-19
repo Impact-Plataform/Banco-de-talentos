54244036e5b3feb33af1dd277e4737c661d55541
filipe <filipef.paulo@gmail.com>
2023-01-18 23:22:41 -0300

    Criação do footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,3 +1,43 @@
-export function Footer() {
-  return <footer>SWAPI Client</footer>;
+import cx from 'classnames';
+import { GithubLogo, LinkedinLogo } from 'phosphor-react';
+
+import { useDarkMode } from '../providers/DarkMode';
+
+interface FooterProps {
+  className?: string;
+}
+
+export function Footer({ className }: FooterProps) {
+  const { darkMode } = useDarkMode();
+
+  return (
+    <footer
+      className={cx('w-full py-4 px-10 h-fit flex flex-col items-center', className, {
+        'bg-gray-800': darkMode,
+        'bg-gray-600': !darkMode,
+      })}
+    >
+      <p className="">SWAPI Client</p>
+      <div className="flex flex-row justify-between w-full mt-2">
+        <a
+          href="https://www.linkedin.com/in/filipefpaulo/"
+          className="flex flex-row"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedinLogo size={20} weight="bold" />
+          filipefpaulo
+        </a>
+        <a
+          href="https://github.com/filipefpaulo/JEDI-banco-de-talentos"
+          className="flex flex-row"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GithubLogo size={20} weight="bold" />
+          source
+        </a>
+      </div>
+    </footer>
+  );
 }
