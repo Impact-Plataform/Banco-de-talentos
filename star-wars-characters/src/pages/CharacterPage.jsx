2722bbe9e7d781aae2e94c1ff207940ab2703e3a
genesis-g101 <gyreplicant@gmail.com>
2023-01-19 07:34:34 -0300

    refatorando o css do character page

--- star-wars-characters/src/pages/CharacterPage.jsx
+++ star-wars-characters/src/pages/CharacterPage.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { CharacterContext } from '../context/CharacterContext';
 import { images } from '../assets/images';
+import { charactersAbout } from '../helpers/data.js';
 
 export const CharacterPage = () => {
   const { allCharacters } = useContext(CharacterContext);
@@ -27,24 +28,30 @@ export const CharacterPage = () => {
   }, []);
 
   return (
-    <div className="container character-page">
-      <>
-        <div className="container character-page">
-          <h1>{character.name}</h1>
-          <div className="info">
-            <ul>
-              <li>height: {character.height}</li>
-              <li>mass: {character.mass}</li>
-              <li>hair color: {character.hair_color}</li>
-              <li>eye_color: {character.eye_color}</li>
-              <li>gender: {character.gender}</li>
-            </ul>
-            <div className="character-page-img">
-              <img src={images.characters[character.name]} alt={character.name} />
-            </div>
-          </div>
+    <div className="character-page">
+      <main className="page-card">
+        <div className="page-img">
+          <img src={images.characters[character.name]} alt={character.name} />
         </div>
-      </>
+        <div className="about">
+          <h1>{character.name}</h1>
+          <p>{charactersAbout.characters[character.name]}</p>
+        </div>
+      </main>
     </div>
   );
 };
+
+// <div className="container character-page">
+// <div className="page-card"></div>
+
+// <div className="about">
+//   <div className="about-img">
+//     <img src={images.characters[character.name]} alt={character.name} />
+//   </div>
+//   <div className="about-text">
+//     <h1>{character.name}</h1>
+//     <p>{charactersAbout.characters[character.name]}</p>
+//   </div>
+// </div>
+// </div>
