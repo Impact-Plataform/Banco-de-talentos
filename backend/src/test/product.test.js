39abcf8ac539273890e8af41126e97f7ee2bba22
cxavier6 <cxavier.reis@gmail.com>
2023-01-19 17:10:51 -0300

    fix: change app import relative path

--- backend/src/test/product.test.js
+++ backend/src/test/product.test.js
@@ -1,5 +1,5 @@
 import request from "supertest"
-import { app } from '../server.js'
+import { app } from "backend/server.js"
 
 describe('Test Product Endpoints', () => {
     it('should create a product', async () => { 
