ae453d7a6cc30268c7c280d708c60c81ab386487
cxavier6 <cxavier.reis@gmail.com>
2023-01-19 16:47:40 -0300

    test: add product endpoints tests

---
+++ backend/src/test/product.test.js
@@ -0,0 +1,51 @@
+import request from "supertest"
+import { app } from '../server.js'
+
+describe('Test Product Endpoints', () => {
+    it('should create a product', async () => { 
+        const res = await request(app)
+            .post('/Products')
+            .send({
+                name: "test-product 1", 
+                category: "test-category 1", 
+                price_BRL: 100
+            })
+
+        expect(res.statusCode).toBe(201)
+    })
+
+    it('should list all products', async () => {
+        const res = await request(app)
+            .get('/Products')
+        
+        expect(res.status).toBe(200)
+    })
+
+    it("should list one product by its id", async () => {
+        const res = await request(app)
+        .get("/Products/63c6feae39259caf2f38316f")
+        
+        expect(res.statusCode).toBe(200)
+        expect(res.body.name).toBe("test-product")
+      })
+
+      it("should update a product", async () => {
+        const res = await request(app)
+          .patch("/Products/63c6feae39259caf2f38316f")
+          .send({
+            name: "test-product 2",
+            category: "test-category 2",
+            price_BRL: 200
+          })
+
+        expect(res.statusCode).toBe(200)
+      })
+
+      it("should delete a product", async () => {
+        const res = await request(app)
+        .delete("/Products/63c6feae39259caf2f38316f")
+
+        expect(res.statusCode).toBe(200)
+      })
+})
+
