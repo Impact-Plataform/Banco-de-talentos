e5fdeaa7e29c2e8fe6c7d798bfa8037b8b2ce434
Marlon <marlon14br@gmail.com>
2023-01-19 16:50:17 -0300

    fix :wrench:

--- frontend/Projeto-FrontEnd-Impact/src/Components/Header/Index.jsx
+++ frontend/Projeto-FrontEnd-Impact/src/Components/Header/Index.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
         <StyledHeader>
 
             <a href="https://pt.wikipedia.org/wiki/Star_Wars"><button className="a__button"> Star Wars {<FcWikipedia />} </button> </a>
-            <a href="https://swapi.dev/"><button className="a__button">API {<FcWikipedia />} </button></a>
+            <a href="https://pt.wikipedia.org/wiki/Interface_de_programa%C3%A7%C3%A3o_de_aplica%C3%A7%C3%B5es"><button className="a__button">API {<FcWikipedia />} </button></a>
             <a href="https://www.linkedin.com/in/marlon-alvss/"><button className="a__button">Marlon Alves {<AiFillLinkedin />} </button></a>
             <a href="https://github.com/Marlonalvss/"><button className="a__button">GitHub {<AiFillGithub />} </button></a>
         </StyledHeader>
