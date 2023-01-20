7da428c17d923cc25e80eb54467ad634ea90a7ae
Bruno Bispo <brunobispoo25@gmail.com>
2023-01-19 22:24:02 -0300

    Criado os testes no caso de uso de listar currency.

---
+++ src/use-cases/currency/getCurrency.spec.ts
@@ -0,0 +1,28 @@
+import { describe, expect, expectTypeOf, it } from 'vitest';
+import { NotFoundError } from '../../utils/apiErrors';
+import { GetCurrency } from './getCurrency';
+
+describe('Validate use case "get currency"', async () => {
+
+	const getCurrency = new GetCurrency();
+
+	it('Should be able to return one curreny', async () => {
+		
+		const currency = await getCurrency.get('usd');
+
+		expectTypeOf(currency).toBeObject();
+		expect(currency).toHaveProperty('code', 'USD');
+		expect(currency).toHaveProperty('name');
+		expect(currency).toHaveProperty('value');		
+	});
+
+	it('Should not be able to return a non-existent symbol', async () => {
+		try {
+			await getCurrency.get('awe');
+		} catch (error) {
+			expect(error).toBeInstanceOf(NotFoundError);
+			expect(error).toHaveProperty('message', 'Simbolo não encontrado');
+		}		
+	});
+
+});
