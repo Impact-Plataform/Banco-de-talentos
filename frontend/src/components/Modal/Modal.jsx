f03479cf65c2eba0d58fa05e6198fe7dc001da89
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-22 15:26:17 -0300

    💄style: estilização com icones e cor do background

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import svg from "../../assets/images/icones/stormtrooper.svg";
+import globe from "../../assets/images/icones/globe.svg"
+import film from "../../assets/images/icones/film.svg"
+import ballon from "../../assets/images/icones/ballon.svg"
+import eye from "../../assets/images/icones/eye.svg"
+import people from "../../assets/images/icones/people.svg"
+import up from "../../assets/images/icones/up.svg"
+import activity from "../../assets/images/icones/activity.svg"
+import cut from "../../assets/images/icones/cut.svg"
+import person from "../../assets/images/icones/person.svg"
+import clipboard from "../../assets/images/icones/clipboard.svg"
 
 const Modal = ({
   nome,
@@ -36,7 +46,7 @@ const Modal = ({
         aria-hidden="true"
       >
         <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
+          <div className="modal-content bg-info">
             <div className="modal-header">
               <h1
                 className="modal-title fs-5"
@@ -54,39 +64,39 @@ const Modal = ({
             </div>
             <div className="modal-body">
               <p className="card-text">
-                Gender: <strong>{genero}</strong>
+              <img src={people} alt="icone de pessoas" /> Gender: <strong>{genero}</strong>
               </p>
               <p className="card-text">
-                Height: <strong>{altura} cm </strong>
+              <img src={up} alt="icone de seta pra cima" />  Height: <strong>{altura}</strong>
               </p>
               <p className="card-text">
-                Mass: <strong>{peso} kg </strong>
+              <img src={clipboard} alt="icone de usuário" /> Mass: <strong>{peso}</strong>
               </p>
               <p className="card-text">
-                Hair color: <strong>{corDoCabelo}</strong>
+              <img src={cut} alt="icone de tesoura" /> Hair color: <strong>{corDoCabelo}</strong>
               </p>
               <p className="card-text">
-                Eye color: <strong>{corDosOlhos}</strong>
+              <img src={eye} alt="icone de olho" /> Eye color: <strong>{corDosOlhos}</strong>
               </p>
               <p className="card-text">
-                Skin color: <strong>{pele}</strong>
+              <img src={person} alt="icone de usuário" />  Skin color: <strong>{pele}</strong>
               </p>
               <p className="card-text">
-                Specie: <strong>{especie}</strong>
+              <img src={ballon} alt="icone de calendário" /> Birth year: <strong>{anoDeNascimento} *</strong>
               </p>
               <p className="card-text">
-                Birth year: <strong>{anoDeNascimento} *</strong>
+              <img src={activity} alt="icone de digital" />  Specie: <strong>{especie}</strong>
               </p>
               <p className="card-text">
-                Homeworld: <strong>{planeta}</strong>
+              <img src={globe} alt="icone de globo" /> Homeworld: <strong>{planeta}</strong>
               </p>
               <p className="card-text">
-                Films: <strong>{filmes}</strong>
+              <img src={film} alt="icone de filme" /> Films: <strong>{filmes}</strong>
               </p>
             </div>
             <div className="modal-footer d-flex flex-column p-0">
             <p className="fst-italic" style={{ fontSize: 13 + "px", textAlign: "justify"}}>
-                * O ano de nascimento do personagem, usa o padrão do universo de
+                * O ano de nascimento do personagem usa o padrão do universo de
                 <strong> BBY</strong> ou <strong>ABY </strong>- Antes da Batalha
                 de Yavin ou Após a Batalha de Yavin. A Batalha de Yavin é uma
                 batalha que ocorre no final do episódio IV de Star Wars: Uma
