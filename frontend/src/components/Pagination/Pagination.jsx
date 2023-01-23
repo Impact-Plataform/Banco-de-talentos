902c5358c466013942b9c6090fb91cae28a32ba0
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2023-01-22 22:41:18 -0300

    💄style: mudança cor dos botões

--- frontend/src/components/Pagination/Pagination.jsx
+++ frontend/src/components/Pagination/Pagination.jsx
@@ -3,34 +3,34 @@ import React from "react";
 const Pagination = ({ anterior, proxima, page }) => {
   return (
     <nav aria-label="Page navigation">
-      <ul className="pagination justify-content-center mb-0">
+      <ul className="pagination justify-content-center mb-0 p-3">
         {page == 1 ? (
           <li className="page-item disabled">
-            <a className="page-link" onClick={anterior}>
-              inicio
+            <a className="page-link bg-warning" onClick={anterior}>
+              início
             </a>
           </li>
         ) : (
           <li className="page-item">
-            <a className="page-link" onClick={anterior} href="#">
+            <a className="page-link bg-warning" onClick={anterior} href="#">
               Anterior
             </a>
           </li>
         )}
         <li className="page-item">
-          <a className="page-link" href="#">
+          <a className="page-link bg-tertiary text-warning" href="#">
             {page}
           </a>
         </li>
         {page == 9 ? (
           <li className="page-item disabled">
-            <a className="page-link" href="#" onClick={proxima}>
+            <a className="page-link bg-warning" href="#" onClick={proxima}>
               Fim
             </a>
           </li>
         ) : (
           <li className="page-item">
-            <a className="page-link" href="#" onClick={proxima}>
+            <a className="page-link bg-warning" href="#" onClick={proxima}>
               Próximo
             </a>
           </li>
