90ca5540e2d3f82e9934f20d86479d64b06d5989
Jean Wilker <jeanwilkerss@gmail.com>
2023-01-23 09:29:50 -0300

    Style the about page and add your route.

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -4,6 +4,7 @@ import Header from '././components/Header/Header';
 import Home from './pages/Home/Home';
 import Character from './pages/Character/Character';
 import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
 
 const Router = () => {
     return (
@@ -13,6 +14,7 @@ const Router = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/character/:id" element={<Character />}/>
+                    <Route path="/about" element={<About />}/>
                     <Route path="/contact" element={<Contact />}/>
                 </Routes>
             </BrowserRouter>
