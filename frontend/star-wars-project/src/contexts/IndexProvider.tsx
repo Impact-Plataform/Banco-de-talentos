40b716f326fff7fadf726e39a840bcf972e50986
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-25 16:56:12 -0300

    feat: add films and species providers to index provider

--- frontend/star-wars-project/src/contexts/IndexProvider.tsx
+++ frontend/star-wars-project/src/contexts/IndexProvider.tsx
@@ -2,12 +2,18 @@ import { ApiinfoProvider } from "./providers/ApiData";
 import { FilterProvider } from "./providers/Filter";
 import { ProviderProps } from "./interfaces/providerProps";
 import { PaginationProvider } from "./providers/Pagination";
+import { SpeciesProvider } from "./providers/Species";
+import { FilmsProvider } from "./providers/Films";
 
 export const IndexProvider = ({ children }: ProviderProps) => {
   return (
     <PaginationProvider>
       <ApiinfoProvider>
-        <FilterProvider>{children}</FilterProvider>
+        <FilterProvider>
+          <FilmsProvider>
+            <SpeciesProvider>{children}</SpeciesProvider>
+          </FilmsProvider>
+        </FilterProvider>
       </ApiinfoProvider>
     </PaginationProvider>
   );
