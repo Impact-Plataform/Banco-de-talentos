240ede31abf8e24146ce8915f94f150b303c5cab
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-25 21:07:17 -0300

    refactor: character card get props by object destructuring instead get each prop

--- frontend/star-wars-project/src/components/characterCard/CharacterCard.tsx
+++ frontend/star-wars-project/src/components/characterCard/CharacterCard.tsx
@@ -4,25 +4,23 @@ import { getEndpoint } from "../../utils/getEndpoint";
 import "./stylesCharacterCard.css";
 
 interface CharacterCardProps {
-  name: string;
-  height: number;
-  mass: number;
-  specie: string;
-  gender: string;
+  // name: string;
+  // height: number;
+  // mass: number;
+  // specie: string;
+  // gender: string;
+  character: any;
 }
 
-export const CharacterCard = ({
-  name,
-  height,
-  mass,
-  specie,
-  gender,
-}: CharacterCardProps) => {
+export const CharacterCard = ({ character }: CharacterCardProps) => {
   const [specieName, setSpecieName] = useState("Unknown");
+  const { name, height, mass, species, gender } = character;
 
   useEffect(() => {
-    specie &&
-      api.get(getEndpoint(specie)).then((res) => setSpecieName(res.data.name));
+    species[0] &&
+      api
+        .get(getEndpoint(species[0]))
+        .then((res) => setSpecieName(res.data.name));
   }, []);
 
   return (
@@ -30,14 +28,6 @@ export const CharacterCard = ({
       <div className="card__info">
         <h3 className="card__info__title">Name:</h3>
         <h4 className="card__info__value">{name}</h4>
-      </div>
-      <div className="card__info">
-        <h3 className="card__info__title">Height:</h3>
-        <h4 className="card__info__value">{height}</h4>
-      </div>
-      <div className="card__info">
-        <h3 className="card__info__title">Mass:</h3>
-        <h4 className="card__info__value">{mass}</h4>
       </div>
       <div className="card__info">
         <h3 className="card__info__title">specie:</h3>
