d86ea1fbd67c5daaf3dbbdae61bbb55ef6e6ef3a
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-25 23:48:12 -0300

    feat: add modal with more informations

--- frontend/star-wars-project/src/components/characterCard/CharacterCard.tsx
+++ frontend/star-wars-project/src/components/characterCard/CharacterCard.tsx
@@ -1,20 +1,16 @@
 import { useState, useEffect } from "react";
 import { api } from "../../services/api";
-import { getEndpoint } from "../../utils/getEndpoint";
+import { getEndpoint } from "../../utils/getEndpointimport { ModalCharacterInfos } from "../modalCharacterInfos/ModalCharacterInfos";
 import "./stylesCharacterCard.css";
 
 interface CharacterCardProps {
-  // name: string;
-  // height: number;
-  // mass: number;
-  // specie: string;
-  // gender: string;
   character: any;
 }
 
 export const CharacterCard = ({ character }: CharacterCardProps) => {
   const [specieName, setSpecieName] = useState("Unknown");
-  const { name, height, mass, species, gender } = character;
+  const { name, species, gender } = character;
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     species[0] &&
@@ -37,7 +33,16 @@ export const CharacterCard = ({ character }: CharacterCardProps) => {
         <h3 className="card__info__title">Gender:</h3>
         <h4 className="card__info__value">{gender}</h4>
       </div>
-      <button className="card__button">SEE MORE</button>
+      <button className="card__button" onClick={() => setModalOpen(true)}>
+        SEE MORE
+      </button>
+      <ModalCharacterInfos
+        character={character}
+        modalOpen={modalOpen}
+        closeModal={() => setModalOpen(false)}
+        specieName={specieName}
+        key={"modal"}
+      />
     </li>
   );
 };
