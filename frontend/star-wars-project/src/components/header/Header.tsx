ff45d37a8837b09c6d5e4e1263e05cdcd263febe
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-27 20:02:32 -0300

    feat: add conditional rendering for chosen character page with a back button

--- frontend/star-wars-project/src/components/header/Header.tsx
+++ frontend/star-wars-project/src/components/header/Header.tsx
@@ -1,14 +1,34 @@
+import { useNavigate } from "react-router-dom";
 import { Filter } from "../filters/Filters";
 import { SearchByName } from "../searchByName/SearchByName";
 import { StarWarsTitle } from "../starWarsTitle/StarWarsTitle";
 import "./stylesHeader.css";
 
-export const Header = () => {
+interface HeaderProps {
+  isCharacterPage?: boolean;
+}
+
+export const Header = ({ isCharacterPage = false }: HeaderProps) => {
+  const navigate = useNavigate();
+  const navigateToAllCharactersPage = () => navigate("/");
+
   return (
     <header className="header__container">
       <StarWarsTitle />
-      <SearchByName />
-      <Filter />
+
+      {!isCharacterPage ? (
+        <>
+          <SearchByName />
+          <Filter />
+        </>
+      ) : (
+        <button
+          className="header__button-back"
+          onClick={navigateToAllCharactersPage}
+        >
+          Back
+        </button>
+      )}
     </header>
   );
 };
