4c513c1205d568f0fcc9c58ef946cd3f079bf38f
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-27 20:23:19 -0300

    refactor: when adapt of and get name from url, it uses '_' instead '-'

--- frontend/star-wars-project/src/utils/adaptNameToUrl.ts
+++ frontend/star-wars-project/src/utils/adaptNameToUrl.ts
@@ -2,7 +2,7 @@ type adaptNameToUrlType = (name: string) => string;
 
 export const adaptNameToUrl: adaptNameToUrlType = (name): string => {
   const nameLowerCase = name.toLowerCase();
-  const replaceSpaceToDash = nameLowerCase.replace(" ", "-");
+  const replaceSpaceToDash = nameLowerCase.replace(" ", "_");
 
   return replaceSpaceToDash;
 };
