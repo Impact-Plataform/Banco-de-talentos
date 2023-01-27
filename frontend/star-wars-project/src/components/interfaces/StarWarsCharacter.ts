48b7e931f44163a1b66992fdcfc1e3e85219cb6d
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-27 19:51:51 -0300

    feat: add conditional rendering for chosen character page with a back button

---
+++ frontend/star-wars-project/src/components/interfaces/StarWarsCharacter.ts
@@ -0,0 +1,18 @@
+export interface StarWarsCharacter {
+  name: string;
+  birth_year: string;
+  eye_color: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  mass: string;
+  skin_color: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  starships: string[];
+  vehicles: string[];
+  url: string;
+  created: string;
+  edited: string;
+}
