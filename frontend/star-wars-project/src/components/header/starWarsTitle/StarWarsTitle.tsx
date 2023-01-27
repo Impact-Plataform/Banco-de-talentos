8b357ebcfeaed19fed1e4e956a1027f07c198f71
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-27 20:29:41 -0300

    feat: when click on star wars title it navigates to main page

--- frontend/star-wars-project/src/components/header/starWarsTitle/StarWarsTitle.tsx
+++ frontend/star-wars-project/src/components/header/starWarsTitle/StarWarsTitle.tsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import "./stylesStarWarsTitle.css";
 
 export const StarWarsTitle = () => {
-  // remember: insert function to sent to characters page when click in title
-  return <h1 className="title">STAR WARS</h1>;
+  const navigate = useNavigate();
+  const navigateToAllCharactersPage = () => navigate("/");
+
+  return (
+    <h1 className="title" onClick={navigateToAllCharactersPage}>
+      STAR WARS
+    </h1>
+  );
 };
