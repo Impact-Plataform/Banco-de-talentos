1968f9e3f4c5d7d9ac0fd197e347e9244829f2ff
User <limasthays@gmail.com>
2023-01-28 15:08:49 -0300

    refact: removing useless code

--- main-project/src/components/filters/FilmsFilter.tsx
+++ main-project/src/components/filters/FilmsFilter.tsx
@@ -1,18 +1,12 @@
 import SelectFilter from '../../SelectFilter'
-import { api } from '../../api/api'
 import { useEffect, useState } from 'react'
-import { Films, IFilm } from 'swapi-ts'
+import { Films } from 'swapi-ts'
 
 export function FilmsFilter() {
 	const [filmsList, setFilmsList] = useState<string[]>([])
 
 	useEffect(() => {
 		async function loadData() {
-			// const apiResponse: IFilm[] = await api
-			// 	.get('/films')
-			// 	.then(({ data }) => data.results)
-			// setFilmsList(() => apiResponse.map((item) => item.title))
-
 			const getFilms = (await Films.find()).resources
 			const dadosTratados = getFilms.map((item) => item.value.title)
 			setFilmsList(dadosTratados)
