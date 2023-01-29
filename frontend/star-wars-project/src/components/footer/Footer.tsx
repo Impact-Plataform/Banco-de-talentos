2cc157df5353c8e09a2f6e5763fe2fcfdef8827f
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-01-29 16:45:30 -0300

    feat: create footer component

---
+++ frontend/star-wars-project/src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import "./stylesFooter.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer_container">
+      <h4 className="footer__info">
+        developed by
+        <a
+          href="https://github.com/leonardo-cabral67"
+          target="_blank"
+          className="footer__info__link"
+        >
+          Leonardo Cabral
+        </a>
+      </h4>
+    </div>
+  );
+};
