683e7a2b8c54c7a6410989160c20f2999d7e0eaa
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-01 20:39:32 -0300

    feat: add character films list

--- frontend/star-wars-project/src/components/choosenCharacterInfo/ChosenCharacterInfo.tsx
+++ frontend/star-wars-project/src/components/choosenCharacterInfo/ChosenCharacterInfo.tsx
@@ -1,9 +1,10 @@
 import "./stylesChosenCharacterInfo.css";
 
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { StarWarsCharacter } from "../interfaces/StarWarsCharacter";
 import { api } from "../../services/api";
 import { getEndpoint } from "../../utils/getEndpoint";
+import axios from "axios";
 
 interface ChosenCharacterInfoProps {
   character: StarWarsCharacter;
@@ -13,8 +14,11 @@ export const ChosenCharacterInfo = ({
   character,
 }: ChosenCharacterInfoProps) => {
   const [specieName, setSpecieName] = useState<string>("loading...");
-  const [filmsNames, setFilms] = useState<string[]>([]);
+  const [filmsNames, setFilmsNames] = useState<string[]>([]);
   const [homeWorldName, setHomeWorldName] = useState<string>("loading...");
+
+  const addFilm = (film: string) =>
+    setFilmsNames((filmsNames) => [...filmsNames, film]);
 
   const {
     name,
@@ -28,7 +32,7 @@ export const ChosenCharacterInfo = ({
     homeworld,
   } = character;
 
-  useEffect(() => {
+  useMemo(() => {
     species.length > 0
       ? api
           .get(getEndpoint(species[0]))
@@ -40,8 +44,17 @@ export const ChosenCharacterInfo = ({
           .get(getEndpoint(homeworld))
           .then((res) => setHomeWorldName(res.data.name))
       : setSpecieName("unknown");
+    films.length > 0 &&
+      axios.all(films).then((allResponses) => {
+        allResponses.forEach((filmResponse) => {
+          api.get(getEndpoint(filmResponse)).then((res) => {
+            addFilm(res.data.title);
+          });
+        });
+      });
   }, []);
 
+  console.log("filmsName --->", filmsNames);
   return (
     <main className="character-info__container">
       <div className="character-info__info">
@@ -59,6 +72,26 @@ export const ChosenCharacterInfo = ({
       <div className="character-info__info">
         <h3 className="character-info__title">Gender:</h3>
         <h4 className="character-info__value">{gender}</h4>
+      </div>
+      <div className="character-info__info character-info__info--movies">
+        <h3 className="character-info__title character-info__title--movie">Movies:</h3>
+        <ul className="character-info_movies_container">
+          {filmsNames.length > 0 ? (
+            filmsNames.map((film, index) => (
+              <li>  
+                {" "}
+                <h5 className="character-info--movie_title">
+                  {film}
+                  {index < filmsNames.length - 1 && (
+                    <span className="character-info--movie-comma"> ;</span>
+                  )}
+                </h5>
+              </li>
+            ))
+          ) : (
+            <h4 className="character-info__value">Unknown</h4>
+          )}
+        </ul>
       </div>
       <div className="character-info__info">
         <h3 className="character-info__title">Specie:</h3>
