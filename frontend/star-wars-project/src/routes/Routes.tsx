dfaadee4fd4ee3a25343033ef3df417beb7c13c2
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-01 21:13:09 -0300

    feat: add animatePresence in Routes

--- frontend/star-wars-project/src/routes/Routes.tsx
+++ frontend/star-wars-project/src/routes/Routes.tsx
@@ -1,12 +1,16 @@
-import { Routes as Switch, Route } from "react-router";
+import { AnimatePresence } from "framer-motion";
+import { Routes as Switch, Route, useLocation } from "react-router";
 import { AllCharacters } from "../views/allCharacters/AllCharacters";
 import { CharacterPage } from "../views/characterPage/CharacterPage";
 
 export const Routes = () => {
+  const location = useLocation();
   return (
-    <Switch>
-      <Route path="/" element={<AllCharacters />} />
-      <Route path="/profile/:character" element={<CharacterPage />} />
-    </Switch>
+    <AnimatePresence>
+      <Switch location={location} key={location.pathname}>
+        <Route path="/" element={<AllCharacters />} />
+        <Route path="/profile/:character" element={<CharacterPage />} />
+      </Switch>
+    </AnimatePresence>
   );
 };
