0a1978cc13aef32c12820c995de9a7bd6681cb82
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-07 18:21:43 -0300

    feat: add get methods to products properties

--- backend/products-api/src/entities/product/Product.ts
+++ backend/products-api/src/entities/product/Product.ts
@@ -1,9 +1,19 @@
 interface ProductProps {
-  name: string,
-  price: number,
-  description: string
+  name: string;
+  price: number;
+  description: string;
 }
 
-export class Product {
+export class  Product {
   constructor(private props: ProductProps) {}
+
+  get name() {
+    return this.props.name;
+  }
+  get price() {
+    return this.props.price;
+  }
+  get description() {
+    return this.props.description;
+  }
 }
