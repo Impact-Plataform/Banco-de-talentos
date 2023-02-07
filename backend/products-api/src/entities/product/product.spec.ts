2b381b61626df2badcee391823f784260e0feb9b
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-07 18:22:18 -0300

    test: add get methods to products properties

--- backend/products-api/src/entities/product/product.spec.ts
+++ backend/products-api/src/entities/product/product.spec.ts
@@ -1,13 +1,27 @@
 import { Product } from "./Product";
 
 describe("Product Model", () => {
-  test("create product", () => {
-    const product = new Product({
-      name: "example",
-      price: 19.9,
-      description: "this is an example",
-    });
+  const product = new Product({
+    name: "example",
+    price: 19.9,
+    description: "this is an example",
+  });
 
+  test("create product", () => {
     expect(product).toBeInstanceOf(Product);
+  });
+
+  test("it returns props with get methods", () => {
+    const [name, price, description] = [
+      product.name,
+      product.price,
+      product.description,
+    ];
+
+    expect([name, price, description]).toEqual([
+      "example",
+      19.9,
+      "this is an example",
+    ]);
   });
 });
