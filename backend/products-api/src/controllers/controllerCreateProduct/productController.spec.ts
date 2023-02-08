387b2b2798313f21f258cfe6d1055aac8cf307bf
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-08 12:56:52 -0300

    test: return http code 400 if a product with repeated name is created

--- backend/products-api/src/controllers/controllerCreateProduct/productController.spec.ts
+++ backend/products-api/src/controllers/controllerCreateProduct/productController.spec.ts
@@ -21,4 +21,19 @@ describe("Controller Create Product", () => {
     const createProduct = await controllerProduct.create(request);
     expect(createProduct.statusCode).toBe(201);
   });
+  test("it should returns a status code 400 if creates a product with repeated name", async () => {
+    const request2: RequestHTTP = {
+      body: {
+        name: "Headphone",
+        price: 400,
+        description: "Headphone good to listen to all genders of musics",
+      },
+    };
+
+    const createProduct = await controllerProduct.create(request);
+
+    const createProductRepeated = await controllerProduct.create(request2);
+
+    expect(createProductRepeated.statusCode).toBe(400);
+  });
 });
