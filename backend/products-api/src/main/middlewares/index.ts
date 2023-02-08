6aa6e4d289cfb35b0c1fe280cd2e83462d8e989f
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-08 17:41:16 -0300

    feat: add cors middleware

--- backend/products-api/src/main/middlewares/index.ts
+++ backend/products-api/src/main/middlewares/index.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import { bodyParser } from "./bodyParser/bodyParser";
+import { cors } from "./cors/cors";
 
 export const middleWares = (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors)
 };
