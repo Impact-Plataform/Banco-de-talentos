28e32feb08fe87a4d7099baaf68d5e4c4637ea78
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-08 11:20:23 -0300

    feat: implement create and findProductByName functions

--- backend/products-api/src/repositories/inMemoryProductRepo/InMemoryProductRepo.ts
+++ backend/products-api/src/repositories/inMemoryProductRepo/InMemoryProductRepo.ts
@@ -1,9 +1,16 @@
+import { Product } from "../../entities/product/Product";
 import { IProductRepository } from "../ports/IProductRepository";
 
 export class InMemoryProductRepo implements IProductRepository {
- 
-  async findProductByName(name: string): Promise<boolean> {
-    return true
+  constructor(private list: Product[]) {}
+
+  async create(product: Product): Promise<void> {
+    this.list.push(product);
   }
-  
+
+  async findProductByName(name: string): Promise<boolean> {
+    const productAlreadyExists = this.list.filter(product => product.name === name)
+
+    return productAlreadyExists.length > 0 ? true : false
+  }
 }
