217ddd237495498a9286ce39c2f420a7c37912fe
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-08 20:08:51 -0300

    feat: apply content-type middleware

--- backend/products-api/src/main/middlewares/index.ts
+++ backend/products-api/src/main/middlewares/index.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import { bodyParser } from "./bodyParser/bodyParser";
 import { cors } from "./cors/cors";
+import { contentType } from "./contentType/contentType";
 
 export const middleWares = (app: Express): void => {
   app.use(bodyParser);
-  app.use(cors)
+  app.use(cors);
+  app.use(contentType);
 };
