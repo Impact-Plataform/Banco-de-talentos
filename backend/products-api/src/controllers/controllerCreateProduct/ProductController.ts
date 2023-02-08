841ea873232a38ed5b442e748d85518274c51fdf
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-08 12:33:48 -0300

    feat: return http code 400 if a product with repeated name is created

--- backend/products-api/src/controllers/controllerCreateProduct/ProductController.ts
+++ backend/products-api/src/controllers/controllerCreateProduct/ProductController.ts
@@ -1,14 +1,16 @@
 import { CreateProduct } from "../../usecases/createProduct/CreateProduct";
 import { HttpResponse } from "../ports/httpResponse";
 import { RequestHTTP } from "../ports/requestHttp";
-import { create } from "../utils/httpHelpers";
+import { clientError, create } from "../utils/httpHelpers";
 
 export class ProductController {
   constructor(private createProduct: CreateProduct) {}
 
   async create(request: RequestHTTP): Promise<HttpResponse> {
-    const createProduct = this.createProduct.execute(request.body);
+    const createProduct = await this.createProduct.execute(request.body);
 
-    return create("created");
+    return createProduct
+      ? create("created")
+      : clientError("already exists a product with this name");
   }
 }
