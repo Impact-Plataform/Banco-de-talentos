e6923b026a0e304a514656283676fd62f119aef7
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-10 18:19:46 -0300

    chore: add and configure jest-mongodb

--- backend/products-api/jest.config.js
+++ backend/products-api/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  roots: ["<rootDir>/test"],
+  preset: '@shelf/jest-mongodb',
+  roots: ['<rootDir>/test'],
   clearMocks: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  collectCoverageFrom: ["<rootDir>/test/**/*.ts"],
-  testEnvironment: "node",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['<rootDir>/test/**/*.ts'],
+  testEnvironment: 'node',
   transform: {
-    ".+\\.ts$": "ts-jest",
-  },
-};
+    '.+\\.ts$': 'ts-jest'
+  }
+}
