10fc50aa60d247905e654420583ee9c2353989d4
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-11 11:47:43 -0300

    fix: connect mongodb before start application server

--- backend/products-api/src/main/server.ts
+++ backend/products-api/src/main/server.ts
@@ -1,5 +1,10 @@
-import app from "./config/app";
+import { MongoHelper } from "../repositories/mongodb/helper/mongoHelper";
 
-app.listen(5000, () => {
-  console.log("Server running at http://localhost:5000");
-});
+MongoHelper.connect("mongodb://localhost:27017/api-products")
+  .then(async () => {
+    const app = (await import("./config/app")).default;
+    app.listen(5000, () => {
+      console.log("Server running at http://localhost:5000");
+    });
+  })
+  .catch((err) => console.log(err));
