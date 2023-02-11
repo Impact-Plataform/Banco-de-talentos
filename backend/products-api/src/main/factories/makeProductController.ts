8d9f75edfe0b4e0714f7dbfaf82a8a31dc7ffe40
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-11 11:46:04 -0300

    fix: use mongodb products repository instead in memory products repository

--- backend/products-api/src/main/factories/makeProductController.ts
+++ backend/products-api/src/main/factories/makeProductController.ts
@@ -1,11 +1,9 @@
 import { ProductController } from "../../controllers/ProductController";
-import { Product } from "../../entities/Product";
-import { InMemoryProductRepo } from "../../repositories/InMemoryProductRepo";
+import { MongodbProductsRepository } from "../../repositories/mongodb/MongodbProductsRepository";
 import { CreateProduct } from "../../usecases/createProduct/CreateProduct";
 
 export const makeProductController = () => {
-  const list: Product[] = [];
-  const productRepository = new InMemoryProductRepo(list);
+  const productRepository = new MongodbProductsRepository();
   const productUsecase = new CreateProduct(productRepository);
   const productController = new ProductController(productUsecase);
 
