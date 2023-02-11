a14f89396367e89efd8d6d48888b9878106785e4
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-11 12:28:18 -0300

    fix: change product controller constructor property name from createProduct to usecaseProduct

--- backend/products-api/src/controllers/ProductController.ts
+++ backend/products-api/src/controllers/ProductController.ts
@@ -4,10 +4,10 @@ import { RequestHTTP } from "./ports/requestHttp";
 import { clientError, create } from "./utils/httpHelpers";
 
 export class ProductController {
-  constructor(private createProduct: UsecaseProduct) {}
+  constructor(private usecaseProduct: UsecaseProduct) {}
 
   async create(request: RequestHTTP): Promise<HttpResponse> {
-    const createProduct = await this.createProduct.create(request.body);
+    const createProduct = await this.usecaseProduct.create(request.body);
 
     return createProduct
       ? create("created")
