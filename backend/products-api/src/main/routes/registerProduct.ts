a85d7019ce6dde17437b92d7f3fa56350bd23388
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-12 12:47:31 -0300

    fix: update import for adaptCreateProductRoute

--- backend/products-api/src/main/routes/registerProduct.ts
+++ backend/products-api/src/main/routes/registerProduct.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { adaptRoute } from "../adapters/expressRouteAdapter";
+import { adaptCreateProductRoute } from "../adapters/expressRouteAdapter";
 import { makeProductController } from "../factories/makeProductController";
 
 export default (router: Router) => {
-  router.post("/Products", adaptRoute(makeProductController()));
+  router.post("/Products", adaptCreateProductRoute(makeProductController()));
 };
