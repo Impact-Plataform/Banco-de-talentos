f2f540ccf34fecb237979d8107bc176c31634b5e
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-12 16:38:13 -0300

    fix: update file name import

--- backend/products-api/src/main/routes/registerProduct.ts
+++ backend/products-api/src/main/routes/registerProduct.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { adaptCreateProductRoute } from "../adapters/expressRouteAdapter";
+import { adaptCreateProductRoute } from "../adapters/adaptCreateProductsRoute";
 import { makeProductController } from "../factories/makeProductController";
 
 export default (router: Router) => {
