915b8745480a8590d06c97ddf7aaf62d4cfdc94e
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-12 12:40:50 -0300

    feat: add getAllProducts method to IProductRepository

--- backend/products-api/src/repositories/ports/IProductRepository.ts
+++ backend/products-api/src/repositories/ports/IProductRepository.ts
@@ -1,6 +1,7 @@
-import { Product, ProductProps } from "../../entities/Product";
+import { ProductProps } from "../../entities/Product";
 
 export interface IProductRepository {
   create(product: ProductProps): Promise<void>;
   findProductByName(name: string): Promise<boolean>;
+  getAllProducts(): Promise<ProductProps[]>
 }
