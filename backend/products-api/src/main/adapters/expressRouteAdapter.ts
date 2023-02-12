f0fa62b8faab4a195811de2152cbc1ddb746d0ca
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-12 12:46:18 -0300

    fix: change adaptRoute name to adaptCreateProductRoute

--- backend/products-api/src/main/adapters/expressRouteAdapter.ts
+++ backend/products-api/src/main/adapters/expressRouteAdapter.ts
@@ -3,7 +3,7 @@ import { HttpResponse } from "../../controllers/ports/httpResponse";
 import { RequestHTTP } from "../../controllers/ports/requestHttp";
 import { ProductController } from "../../controllers/ProductController";
 
-export const adaptRoute = (controller: ProductController) => {
+export const adaptCreateProductRoute = (controller: ProductController) => {
   return async (req: Request, res: Response) => {
     const httpRequest: RequestHTTP = {
       body: req.body,
