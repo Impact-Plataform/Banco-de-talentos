6526a4c93b6ff635a3688948c68433120dc74a77
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-13 02:57:34 -0300

    feat: adapt delete producy by name route

---
+++ backend/products-api/src/main/adapters/adaptDeleteProductByNameRoute.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { HttpResponse } from "../../controllers/ports/httpResponse";
+import { RequestHTTP } from "../../controllers/ports/requestHttp";
+import { ProductController } from "../../controllers/ProductController";
+
+export const adapteDeleteProductByNameRoute = (
+  controller: ProductController
+) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: RequestHTTP = {
+      body: req.body,
+    };
+    const httpResponse: HttpResponse = await controller.deleteProduct(
+      httpRequest
+    );
+    const { statusCode, body } = httpResponse;
+    res.status(statusCode).json(body);
+  };
+};
