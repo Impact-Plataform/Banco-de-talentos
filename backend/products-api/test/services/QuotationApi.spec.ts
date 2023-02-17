474feef112eb05b16e15970cc7fc22b257f09184
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-16 22:02:13 -0300

    test: delete static before getAPIQuotationInfo

--- backend/products-api/test/services/QuotationApi.spec.ts
+++ backend/products-api/test/services/QuotationApi.spec.ts
@@ -2,7 +2,8 @@ import { QuotationApi } from "../../src/services/QuotationApi";
 
 describe("Quotation api service", () => {
   test("get dollar currency info from url", async () => {
-    const dollarInfo = await QuotationApi.getAPIQuotationInfo("/USD-BRL");
+    const quotationApi = new QuotationApi();
+    const dollarInfo = await quotationApi.getAPIQuotationInfo("/USD-BRL");
     console.log(dollarInfo);
     expect(dollarInfo).toBeTruthy();
   });
