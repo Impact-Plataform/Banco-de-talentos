1fcb06c8a60c233b64c317c5059008bc9832e20e
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-16 22:07:59 -0300

    test: create method returns currency, createCurrencyBase method and implement createDollar

--- backend/products-api/test/usecases/UseCaseCurrency.spec.ts
+++ backend/products-api/test/usecases/UseCaseCurrency.spec.ts
@@ -1,11 +1,12 @@
 import { CurrencyProps } from "../../src/entities/Currency";
 import { InMemoryCurrencyRepo } from "../../src/repositories/InMemoryCurrencyRepo";
+import { QuotationApi } from "../../src/services/QuotationApi";
 import { UseCaseCurrency } from "../../src/usecases/UseCaseCurrency";
 
 describe("Use case currency", () => {
   const currencyProps: CurrencyProps = {
     code: "USD",
-    codeIn: "BRL",
+    codein: "BRL",
     name: "Dólar Americano/Real Brasileiro",
     high: "5",
   };
@@ -13,9 +14,19 @@ describe("Use case currency", () => {
   test("can create a currency", async () => {
     const currencyList: CurrencyProps[] = [];
     const repository = new InMemoryCurrencyRepo(currencyList);
-    const currencyUsecase = new UseCaseCurrency(repository);
+    const service = new QuotationApi();
+    const currencyUsecase = new UseCaseCurrency(repository, service);
 
     const createCurrency = await currencyUsecase.create(currencyProps);
-    expect(currencyList[0]).toEqual(currencyProps);
+    expect(createCurrency).toEqual(currencyProps);
+  });
+
+  test("Should create a currency getting dollar informations from quotation api", async () => {
+    const currencyList: CurrencyProps[] = [];
+    const repository = new InMemoryCurrencyRepo(currencyList);
+    const service = new QuotationApi();
+    const currencyUsecase = new UseCaseCurrency(repository, service);
+    const usd = await currencyUsecase.createDollar();
+    expect(usd).toBeTruthy();
   });
 });
