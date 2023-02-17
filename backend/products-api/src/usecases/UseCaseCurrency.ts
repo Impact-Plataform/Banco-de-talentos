688580276bfae5075b7f541bd0d852164b936d7a
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-16 22:05:00 -0300

    fix: create method returns currency, createCurrencyBase method and implement createDollar

--- backend/products-api/src/usecases/UseCaseCurrency.ts
+++ backend/products-api/src/usecases/UseCaseCurrency.ts
@@ -1,9 +1,27 @@
 import { CurrencyProps } from "../entities/Currency";
 import { ICurrencyRepository } from "../repositories/ports/ICurrencyRepository";
+import { QuotationApi } from "../services/QuotationApi";
 
 export class UseCaseCurrency {
-  constructor(private repository: ICurrencyRepository) {}
+  constructor(
+    private repository: ICurrencyRepository,
+    private quotationService: QuotationApi
+  ) {}
   async create(currency: CurrencyProps) {
     this.repository.create(currency);
+    return currency;
+  }
+
+  async createCurrencyBase(currencySymbol: string) {
+    const getCurrencyInfo = await this.quotationService.getAPIQuotationInfo(
+      `/${currencySymbol}-BRL`
+    );
+    const { code, codein, name, high } = getCurrencyInfo[currencySymbol];
+    const currencyInfo = { code, codein, name, high };
+    return await this.create(currencyInfo);
+  }
+
+  async createDollar() {
+    return await this.createCurrencyBase("USD");
   }
 }
