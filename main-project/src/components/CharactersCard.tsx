66c9c2bb49ea83ea1aa225d4e6a5863578db4f44
User <limasthays@gmail.com>
2023-02-19 12:54:22 -0300

    updates on single character cards style

--- main-project/src/components/CharactersCard.tsx
+++ main-project/src/components/CharactersCard.tsx
@@ -9,27 +9,22 @@ type characterType = {
 
 export function CharactersCard({ character, birthplace, info }: characterType) {
 	return (
-		<>
-			<div className="border border-white rounded-md py-2 px-3 flex flex-col w-[70%]">
-				<img
-					src={require(`../assets/characters-images/${info.id}.jpg`)}
-					alt={`${info.name}'s on schene`}
-				/>
+		<div className="border border-white rounded-md py-2 px-3 flex flex-col items-center gap-3 w-[70%]">
+			<img
+				src={require(`../assets/characters-images/${info.id}.jpg`)}
+				alt={`${info.name}'s on schene`}
+			/>
+
+			<div className="text-center">
 				<h3>{character.name}</h3>
-				<span className="font-bold">
+				<span className="text-sm">
 					{character.gender}, from {birthplace}
 				</span>
-				<div className="text-xs">
-					<p>heigth: {character.height}</p>
-					<p>hair color: {character.hair_color}</p>
-					<p>eye color: {character.eye_color}</p>
-					<p>birth year: {character.birth_year}</p>
-				</div>
-
-				<Link to={`/character/${info.name}`}>
-					<button className="font-bold">+ click for more</button>
-				</Link>
 			</div>
-		</>
+
+			<Link to={`/character/${info.name}`}>
+				<button className="font-bold">+ click for more</button>
+			</Link>
+		</div>
 	)
 }
