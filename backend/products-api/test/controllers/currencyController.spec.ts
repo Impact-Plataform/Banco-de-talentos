de537260be1bab046b4340e49ed2baf68cffe41a
Leonardo Cabral <leonardoleocabral@outlook.com>
2023-02-21 11:51:43 -0300

    test: get all currencies method

---
+++ backend/products-api/test/controllers/currencyController.spec.ts
@@ -0,0 +1,17 @@
+import { CurrencyController } from "../../src/controllers/CurrencyController";
+import { CurrencyProps } from "../../src/entities/Currency";
+import { InMemoryCurrencyRepo } from "../../src/repositories/InMemoryCurrencyRepo";
+import { QuotationApi } from "../../src/services/QuotationApi";
+import { UseCaseCurrency } from "../../src/usecases/UseCaseCurrency";
+
+describe("Currency controller", () => {
+  test("should get all currencies", async () => {
+    const currenciesList: CurrencyProps[] = [];
+    const repository = new InMemoryCurrencyRepo(currenciesList);
+    const service = new QuotationApi();
+    const usecaseCurrency = new UseCaseCurrency(repository, service);
+    const currencyController = new CurrencyController(usecaseCurrency);
+    const allCurrencies = await currencyController.getCurrencies();
+    expect(allCurrencies.body.currencies).toBeTruthy();
+  });
+});
