387641d77683b7b1ad358693119b5f77486e3683
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-02-22 23:07:57 +0000

    test: cache repo to contructor and implement set and get functions

--- backend/products-api/test/controllers/currencyController.spec.ts
+++ backend/products-api/test/controllers/currencyController.spec.ts
@@ -1,16 +1,20 @@
 import { CurrencyController } from "../../src/controllers/CurrencyController";
 import { CurrencyProps } from "../../src/entities/Currency";
 import { InMemoryCurrencyRepo } from "../../src/repositories/inMemory/InMemoryCurrencyRepo";
+import { client } from "../../src/repositories/redis/client/redisClient";
+import { RedisCache } from "../../src/repositories/redis/RedisCache";
 import { QuotationApi } from "../../src/services/QuotationApi";
 import { UseCaseCurrency } from "../../src/usecases/UseCaseCurrency";
 
 describe("Currency controller", () => {
+  beforeAll(() => client.connect())
   test("should get all currencies", async () => {
     const currenciesList: CurrencyProps[] = [];
     const repository = new InMemoryCurrencyRepo(currenciesList);
     const service = new QuotationApi();
     const usecaseCurrency = new UseCaseCurrency(repository, service);
-    const currencyController = new CurrencyController(usecaseCurrency);
+    const redis = new RedisCache()
+    const currencyController = new CurrencyController(usecaseCurrency, redis);
     const allCurrencies = await currencyController.getCurrencies();
     expect(allCurrencies.body.currencies).toBeTruthy();
   });
