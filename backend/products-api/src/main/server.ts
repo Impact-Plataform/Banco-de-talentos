4c9a83927e3e9d4d913571928e936b5d7e21fa1c
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-02-22 19:41:58 +0000

    fix: get url from codespaces secret environment variable and change message from console.log

--- backend/products-api/src/main/server.ts
+++ backend/products-api/src/main/server.ts
@@ -1,10 +1,10 @@
 import { MongoHelper } from "../repositories/mongodb/helper/mongoHelper";
 
-MongoHelper.connect("mongodb://localhost:27017/api-products")
+MongoHelper.connect(process.env.MONGODB_URL)
   .then(async () => {
     const app = (await import("./config/app")).default;
     app.listen(5000, () => {
-      console.log("Server running at http://localhost:5000");
+      console.log("Server running at https://leonardo-cabral67-friendly-invention-w44vrj4grqph5vv-5000.preview.app.github.dev/");
     });
   })
   .catch((err) => console.log(err));
