d6b31b709c3fd109ff436a7c467a809f281c668e
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-02-23 02:59:34 +0000

    feat: add quotation service to product controller factory

--- backend/products-api/src/main/factories/makeProductController.ts
+++ backend/products-api/src/main/factories/makeProductController.ts
@@ -1,10 +1,12 @@
 import { ProductController } from "../../controllers/ProductController";
 import { MongodbProductsRepository } from "../../repositories/mongodb/MongodbProductsRepository";
+import { QuotationApi } from "../../services/QuotationApi";
 import { UsecaseProduct } from "../../usecases/UsecaseProduct";
 
 export const makeProductController = () => {
   const productRepository = new MongodbProductsRepository();
-  const productUsecase = new UsecaseProduct(productRepository);
+  const quotationService = new QuotationApi()
+  const productUsecase = new UsecaseProduct(productRepository, quotationService);
   const productController = new ProductController(productUsecase);
 
   return productController;
