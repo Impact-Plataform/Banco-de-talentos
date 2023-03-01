55512218cbec1d5c24da1759bd14c5279e334602
Leonardo Cabral <80075113+leonardo-cabral67@users.noreply.github.com>
2023-03-01 13:59:51 +0000

    fix: could not test redis connection

--- backend/products-api/test/main/routes/getAllCurrencies.spec.ts
+++
@@ -1,8 +0,0 @@
-import request from "supertest";
-import app from "../../../src/main/config/app";
-
-describe("Get all currencies route", () => {
-  test("should return all currencies", async () => {
-    await request(app).get("/Currency").expect(200);
-  });
-});
