93a69db0062bf6c332b1663adfec1c39ad351a82
User <limasthays@gmail.com>
2023-03-08 17:18:25 -0300

    removing pagination button when a filter is setted

--- main-project/src/components/GroupPaginationButtons.tsx
+++ main-project/src/components/GroupPaginationButtons.tsx
@@ -3,12 +3,14 @@ import { SWContext } from '../contexts/SWContext'
 import { PaginationButton } from './PaginationButton'
 
 export const GroupPaginationButtons = () => {
-	const { pagination, next, previous, setPagination } = useContext(SWContext)
+	const { pagination, next, previous, setPagination, filterValue } =
+		useContext(SWContext)
 	return (
 		<div className="flex justify-evenly">
 			<PaginationButton
 				title="previous"
 				disabled={Boolean(!previous)}
+				isButtonVisible={!Boolean(filterValue !== 'all')}
 				callback={() => {
 					if (previous) {
 						setPagination(pagination! - 1)
@@ -19,6 +21,7 @@ export const GroupPaginationButtons = () => {
 			<PaginationButton
 				title="next"
 				disabled={Boolean(!next)}
+				isButtonVisible={!Boolean(filterValue !== 'all')}
 				callback={() => {
 					if (next) {
 						setPagination(pagination! + 1)
--- main-project/src/components/PaginationButton.tsx
+++ main-project/src/components/PaginationButton.tsx
@@ -1,13 +1,17 @@
+import clsx from 'clsx'
+
 type PaginationButtonProps = {
 	title: string
 	callback: Function
 	disabled: boolean
+	isButtonVisible?: boolean
 }
 
 export function PaginationButton({
 	title,
 	callback,
 	disabled,
+	isButtonVisible,
 }: PaginationButtonProps) {
 	return (
 		<button
@@ -15,7 +19,10 @@ export function PaginationButton({
 				callback()
 			}}
 			disabled={disabled}
-			className="border border-white bg-transparent text-xs py-1 px-2 rounded-md"
+			className={clsx(
+				'border border-white bg-transparent text-xs py-1 px-2 rounded-md',
+				{ hidden: isButtonVisible === false }
+			)}
 		>
 			{title}
 		</button>
