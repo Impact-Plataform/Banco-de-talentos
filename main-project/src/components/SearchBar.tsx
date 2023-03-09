f79d39de18d5da57c057b8c4a4473356ac20be74
User <limasthays@gmail.com>
2023-03-08 22:39:20 -0300

    fixing bugs on filters rules

--- main-project/src/components/SearchBar.tsx
+++ main-project/src/components/SearchBar.tsx
@@ -2,7 +2,8 @@ import { useContext } from 'react'
 import { SWContext } from '../contexts/SWContext'
 
 export function SearchBar() {
-	const { setSearchByName } = useContext(SWContext)
+	const { setSearchByName, setFilterType, setFilterValue } =
+		useContext(SWContext)
 	return (
 		<input
 			type="text"
@@ -10,6 +11,12 @@ export function SearchBar() {
 			placeholder="search by name..."
 			onChange={(event) => {
 				setSearchByName(event.target.value)
+
+				setFilterValue('all')
+				if (event.target.value === '') {
+					setFilterType('all')
+					setFilterValue('all')
+				}
 			}}
 		/>
 	)
