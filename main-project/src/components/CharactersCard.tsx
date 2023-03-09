986581b97c7482aac0ed23322274acb74fe39f3f
User <limasthays@gmail.com>
2023-03-08 21:42:28 -0300

    adding conditional render on characters card

--- main-project/src/components/CharactersCard.tsx
+++ main-project/src/components/CharactersCard.tsx
@@ -21,25 +21,29 @@ export function CharactersCard({ character, birthplace, info }: characterType) {
 			whileHover={{ scale: 1.05 }}
 			className="bg-black border border-white hover:border-primary rounded-md py-2 px-3 flex flex-col items-center gap-3 w-[80%] md:w-[25%]"
 		>
-			<Link to={`/character/${info.name}`}>
-				<img
-					src={require(`../assets/characters-images/${info.id}.jpg`)}
-					alt={`${info.name}'s on schene`}
-				/>
-			</Link>
+			{character && (
+				<>
+					<Link to={`/character/${info.name}`}>
+						<img
+							src={require(`../assets/characters-images/${info.id}.jpg`)}
+							alt={`${info.name}'s on schene`}
+						/>
+					</Link>
 
-			<div className="text-center">
-				<h3 className="text-primary">{character.name}</h3>
-				<span className="text-sm">
-					{modifyGender(character.gender)}, from {birthplace}
-				</span>
-			</div>
+					<div className="text-center">
+						<h3 className="text-primary">{character.name}</h3>
+						<span className="text-sm">
+							{modifyGender(character.gender)}, from {birthplace}
+						</span>
+					</div>
 
-			<Link to={`/character/${info.name}`}>
-				<button className="font-bold hover:text-primary">
-					+ click for more
-				</button>
-			</Link>
+					<Link to={`/character/${info.name}`}>
+						<button className="font-bold hover:text-primary">
+							+ click for more
+						</button>
+					</Link>
+				</>
+			)}
 		</motion.div>
 	)
 }
