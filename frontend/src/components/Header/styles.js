afeee256df571d9b2bdb422780b5f0f283e10f5c
salomaosilval <salomaosilval@hotmail.com>
2023-03-28 15:47:50 -0400

    :sparkles: feat: add Header

---
+++ frontend/src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100vw;
+  height: 8rem;
+  background-color: #000;
+  border-bottom: 1px solid #fff;
+
+  img {
+    width: 12rem;
+  }
+`;
