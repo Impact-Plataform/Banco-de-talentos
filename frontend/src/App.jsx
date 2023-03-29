dd61863f1a43ac9567ac7a841bd04ac7a6873c55
salomaosilval <salomaosilval@hotmail.com>
2023-03-29 18:16:14 -0400

    :sparkles: feat: add all routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Character } from "./pages/Character";
+import { Home } from "./pages/Home";
+
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Desafio plataforma impact</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path={`/character/:name`} element={<Character />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
